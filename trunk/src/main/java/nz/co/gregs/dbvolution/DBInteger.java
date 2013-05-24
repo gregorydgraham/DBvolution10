efba44d21f188068c1f6d380f1d4a97f64146b23
gregorydgraham <gregorydgraham@bdf2cdf3-4205-4e92-8bbf-280280a2a4f9>
2013-05-24 03:34:26 +0000

    Corrected structure

---
+++ trunk/src/main/java/nz/co/gregs/dbvolution/DBInteger.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nz.co.gregs.dbvolution;
+
+/**
+ *
+ * @author gregory.graham
+ */
+public class DBInteger extends DBNumber {
+
+    private static final long serialVersionUID = 1L;
+    
+    public DBInteger(int anInt) {
+        super(Integer.valueOf(anInt).longValue());
+    }
+
+    public DBInteger(Integer anInt) {
+        super(anInt.longValue());
+    }
+
+    public DBInteger(Long aLong) {
+        super(aLong);
+    }
+
+    public DBInteger() {
+        super();
+    }
+
+    @Override
+    public String getSQLDatatype() {
+        return "INTEGER";
+    }
+}
