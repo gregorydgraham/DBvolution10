6cb60705bffa4590c9e4cff034a99be397185c31
gregorydgraham <gregorydgraham@bdf2cdf3-4205-4e92-8bbf-280280a2a4f9>
2013-07-07 04:19:09 +0000

    [maven-release-plugin]  copy for tag dbvolution-0.6.5

---
+++ tags/dbvolution-0.6.5/src/main/java/nz/co/gregs/dbvolution/operators/DBExistsOperator.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2013 gregory.graham.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nz.co.gregs.dbvolution.operators;
+
+import java.beans.IntrospectionException;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import nz.co.gregs.dbvolution.DBTable;
+import nz.co.gregs.dbvolution.databases.DBDatabase;
+import nz.co.gregs.dbvolution.DBTableRow;
+import nz.co.gregs.dbvolution.QueryableDatatype;
+
+/**
+ *
+ * @author gregory.graham
+ */
+public class DBExistsOperator extends DBOperator {
+
+    DBTableRow tableRow;
+    private final String referencedColumnName;
+
+    public DBExistsOperator(DBTableRow tableRow, QueryableDatatype qdtOfTheRow) throws IllegalArgumentException, IllegalAccessException {
+        this.tableRow = tableRow;
+        Field qdtField = tableRow.getFieldOf(qdtOfTheRow);
+        if (qdtField == null) {
+            throw new RuntimeException("QueryableDatatype Not Found: the specified " + qdtOfTheRow.getClass().getSimpleName() + " is not part of the specified row, please use only columns from the actual row");
+        }
+        this.referencedColumnName = tableRow.getDBColumnName(qdtField);
+    }
+    
+    
+
+    @Override
+    public String generateWhereLine(DBDatabase database, String columnName) {
+        DBTable<DBTableRow> table = new DBTable<DBTableRow>(database, tableRow);
+        String subSelect;
+        try {
+            subSelect = table.getSelectStatementForWhereClause() + table.getWhereClauseWithExampleAndRawSQL(tableRow, " and " + columnName + " = " + referencedColumnName);
+        } catch (IllegalArgumentException ex) {
+            throw new RuntimeException("Error In DBExistsOperator", ex);
+        } catch (IllegalAccessException ex) {
+            throw new RuntimeException("Error In DBExistsOperator", ex);
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error In DBExistsOperator", ex);
+        } catch (InstantiationException ex) {
+            throw new RuntimeException("Error In DBExistsOperator", ex);
+        } catch (NoSuchMethodException ex) {
+            throw new RuntimeException("Error In DBExistsOperator", ex);
+        } catch (InvocationTargetException ex) {
+            throw new RuntimeException("Error In DBExistsOperator", ex);
+        } catch (ClassNotFoundException ex) {
+            throw new RuntimeException("Error In DBExistsOperator", ex);
+        } catch (IntrospectionException ex) {
+            throw new RuntimeException("Error In DBExistsOperator", ex);
+        }
+
+        return database.beginAndLine() + (invertOperator?" not ":"")+" exists (" + subSelect + ") ";
+    }
+}
---
+++ tags/dbvolution-0.6.5/src/test/java/nz/co/gregs/dbvolution/h2/DBTableDeleteTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2013 gregory.graham.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nz.co.gregs.dbvolution.h2;
+
+import java.beans.IntrospectionException;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import nz.co.gregs.dbvolution.example.Marque;
+
+/**
+ *
+ * @author gregory.graham
+ */
+public class DBTableDeleteTest extends AbstractTest {
+
+    public DBTableDeleteTest(String testName) {
+        super(testName);
+    }
+
+    public void testDeleteListOfRows() throws IllegalArgumentException, IllegalAccessException, IntrospectionException, InvocationTargetException, SQLException, InstantiationException, NoSuchMethodException {
+        marques.getAllRows();
+        ArrayList<Marque> rowList = marques.toList();
+        int originalSize = rowList.size();
+        System.out.println("rowList.size()==" + rowList.size());
+        ArrayList<Marque> deleteList = new ArrayList<Marque>();
+        for (Marque row : rowList) {
+            if (row.getIsUsedForTAFROs().toString().equals("False")) {
+                deleteList.add(row);
+            }
+        }
+        marques.delete(deleteList);
+        marques.getAllRows();
+        System.out.println("rowList.size()==" + marques.toList().size());
+        assertTrue("All 'False' rows have not been deleted", originalSize - deleteList.size() == marques.toList().size());
+    }
+}
