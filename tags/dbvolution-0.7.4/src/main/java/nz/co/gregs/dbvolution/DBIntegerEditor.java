387b7916850b05650077f23222891b115664ceeb
gregorydgraham <gregorydgraham@bdf2cdf3-4205-4e92-8bbf-280280a2a4f9>
2013-07-17 11:36:41 +0000

    [maven-release-plugin]  copy for tag dbvolution-0.7.4

---
+++ tags/dbvolution-0.7.4/src/main/java/nz/co/gregs/dbvolution/DBIntegerEditor.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nz.co.gregs.dbvolution;
+
+import java.beans.PropertyEditorSupport;
+
+/**
+ *
+ * @author gregory.graham
+ */
+public class DBIntegerEditor extends PropertyEditorSupport {
+
+    private String format;
+
+    /**
+     *
+     * @param format
+     */
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    /**
+     *
+     * @param text
+     */
+    @Override
+    public void setAsText(String text) {
+        DBInteger type;
+        if (text == null || text.isEmpty()) {
+            type = new DBInteger();
+        } else {
+            type = new DBInteger(text);
+        }
+        setValue(type);
+    }
+}
---
+++ tags/dbvolution-0.7.4/src/main/java/nz/co/gregs/dbvolution/DBNumberEditor.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nz.co.gregs.dbvolution;
+
+import java.beans.PropertyEditorSupport;
+
+/**
+ *
+ * @author gregory.graham
+ */
+public class DBNumberEditor extends PropertyEditorSupport {
+
+    private String format;
+
+    /**
+     *
+     * @param format
+     */
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    /**
+     *
+     * @param text
+     */
+    @Override
+    public void setAsText(String text) {
+        DBNumber type;
+        if (text == null || text.isEmpty()) {
+            type = new DBInteger();
+        } else {
+            type = new DBNumber(text);
+        }
+        setValue(type);
+    }
+}
