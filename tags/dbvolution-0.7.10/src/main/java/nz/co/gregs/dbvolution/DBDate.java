2eb809430b1d17c7282847809997c35702314445
gregorydgraham <gregorydgraham@bdf2cdf3-4205-4e92-8bbf-280280a2a4f9>
2013-08-02 10:11:46 +0000

    [maven-release-plugin]  copy for tag dbvolution-0.7.10

---
+++ tags/dbvolution-0.7.10/src/main/java/nz/co/gregs/dbvolution/DBDate.java
@@ -0,0 +1,148 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nz.co.gregs.dbvolution;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+import nz.co.gregs.dbvolution.operators.DBLikeOperator;
+
+/**
+ *
+ * @author gregory.graham
+ */
+public class DBDate extends QueryableDatatype {
+
+    private static final long serialVersionUID = 1L;
+    protected Date dateValue = null;
+
+    public DBDate() {
+        super();
+    }
+
+    public DBDate(Date date) {
+        super(date);
+        if (date == null) {
+            this.isDBNull = true;
+        } else {
+            dateValue = date;
+        }
+//        this.isLiterally(date);
+    }
+
+    DBDate(Timestamp timestamp) {
+        super(timestamp);
+        if (timestamp == null) {
+            this.isDBNull = true;
+        } else {
+            Date date = new Date();
+            date.setTime(timestamp.getTime());
+            dateValue = date;
+//            this.isLiterally(dateValue);
+        }
+    }
+
+    DBDate(String str) {
+        final long dateLong = Date.parse(str);
+        dateValue = new Date();
+        dateValue.setTime(dateLong);
+        this.isLiterally(dateValue);
+    }
+
+    @Override
+    public void blankQuery() {
+        super.blankQuery();
+        this.dateValue = null;
+    }
+
+    @Override
+    public String getWhereClause(String columnName) {
+//        if (this.usingLikeComparison) {
+        if (this.getOperator() instanceof DBLikeOperator) {
+            throw new RuntimeException("DATE COLUMNS CAN'T USE \"LIKE\": " + columnName);
+        } else {
+            return super.getWhereClause(columnName);
+        }
+//        return whereClause.toString();
+    }
+    
+    public Date dateValue(){
+        return dateValue;
+    }
+
+    public void isLiterally(Date date) {
+        super.isLiterally(date);
+        dateValue = date;
+    }
+
+    public void isLiterally(String dateStr) {
+        final long dateLong = Date.parse(dateStr);
+        Date date = new Date();
+        date.setTime(dateLong);
+        super.isLiterally(date);
+        dateValue = date;
+    }
+
+    @Override
+    public void isLike(Object obj) {
+        throw new RuntimeException("LIKE Comparison Cannot Be Used With Date Fields: " + obj);
+    }
+
+    /**
+     *
+     * @param lower
+     * @param upper
+     */
+    public void isBetween(Date lower, Date upper) {
+        DBDate lowerDate = new DBDate(lower);
+        DBDate upperDate = new DBDate(upper);
+        super.isBetween(lowerDate, upperDate);
+    }
+
+    /**
+     *
+     * @param dates
+     */
+    public void isIn(Date... dates) {
+        ArrayList<DBDate> dbDates = new ArrayList<DBDate>();
+        for (Date date : dates) {
+            dbDates.add(new DBDate(date));
+        }
+        super.isIn(dbDates.toArray(new DBDate[]{}));
+    }
+
+    @Override
+    public String getSQLDatatype() {
+        return "TIMESTAMP";
+    }
+
+    @Override
+    public String toString() {
+        if (this.isDBNull || dateValue == null) {
+            return "";
+        }
+        return dateValue.toString();
+    }
+
+    @Override
+    public String toSQLString() {
+        if (this.isDBNull || dateValue == null) {
+            return this.database.getNull();
+        }
+        return getDatabase().getDateFormattedForQuery(this.dateValue);
+    }
+
+    @Override
+    public String getSQLValue() {
+        return database.getDateFormattedForQuery(dateValue);
+    }
+
+    @Override
+    protected void setFromResultSet(ResultSet resultSet, String fullColumnName) throws SQLException {
+        this.isLiterally(resultSet.getDate(fullColumnName));
+    }
+}
---
+++ tags/dbvolution-0.7.10/src/test/java/nz/co/gregs/dbvolution/h2/AbstractTest.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2013 gregorygraham.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nz.co.gregs.dbvolution.h2;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import net.sourceforge.tedhi.FlexibleDateFormat;
+import net.sourceforge.tedhi.FlexibleDateRangeFormat;
+import nz.co.gregs.dbvolution.DBTable;
+import nz.co.gregs.dbvolution.databases.DBDatabase;
+import nz.co.gregs.dbvolution.databases.H2DB;
+import nz.co.gregs.dbvolution.example.CarCompany;
+import nz.co.gregs.dbvolution.example.Marque;
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ *
+ * @author gregorygraham
+ */
+public class AbstractTest {
+
+    DBDatabase myDatabase = new H2DB("jdbc:h2:~/dbvolutionTest", "", "");
+    Marque myMarqueRow = new Marque();
+    CarCompany myCarCompanyRow = new CarCompany();
+    DBTable<Marque> marques;
+    DBTable<CarCompany> carCompanies;
+    public List<Marque> marqueRows = new ArrayList<Marque>();
+    public List<CarCompany> carTableRows = new ArrayList<CarCompany>();
+    
+    static final FlexibleDateFormat tedhiFormat = FlexibleDateFormat.getPatternInstance("dd/M/yyyy");
+    static final FlexibleDateRangeFormat tedhiRangeFormat = FlexibleDateRangeFormat.getPatternInstance("dd/M/yyyy");
+    public String firstDateStr = "23 March 2013";
+    public String secondDateStr = "2 April 2013";
+
+    @Before
+    @SuppressWarnings("empty-statement")
+    public void setUp() throws Exception {
+
+        myDatabase.setPrintSQLBeforeExecuting(false);
+        myDatabase.dropTableNoExceptions(new Marque());
+        myDatabase.createTable(myMarqueRow);
+
+        myDatabase.dropTableNoExceptions(myCarCompanyRow);
+        myDatabase.createTable(myCarCompanyRow);
+
+        DBTable.setPrintSQLBeforeExecuting(false);
+        marques = new DBTable<Marque>(myDatabase, myMarqueRow);
+        carCompanies = new DBTable<CarCompany>(myDatabase, myCarCompanyRow);
+        carTableRows.add(new CarCompany("TOYOTA", 1));
+        carTableRows.add(new CarCompany("FORD", 2));
+        carTableRows.add(new CarCompany("GENERAL MOTORS", 3));
+        carTableRows.add(new CarCompany("OTHER", 4));
+        carCompanies.insert(carTableRows);
+
+        Date firstDate = tedhiFormat.parse(firstDateStr);
+        Date secondDate = tedhiFormat.parse(secondDateStr);
+
+
+        marqueRows.add(new Marque(4893059, "True", 1246974, null, 3, "UV", "PEUGEOT", null, "Y", null, 4));
+        marqueRows.add(new Marque(4893090, "False", 1246974, "", 1, "UV", "FORD", "", "Y", firstDate, 2));
+        marqueRows.add(new Marque(4893101, "False", 1246974, "", 2, "UV", "HOLDEN", "", "Y", firstDate, 3));
+        marqueRows.add(new Marque(4893112, "False", 1246974, "", 2, "UV", "MITSUBISHI", "", "Y", firstDate, 4));
+        marqueRows.add(new Marque(4893150, "False", 1246974, "", 3, "UV", "SUZUKI", "", "Y", firstDate, 4));
+        marqueRows.add(new Marque(4893263, "False", 1246974, "", 2, "UV", "HONDA", "", "Y", firstDate, 4));
+        marqueRows.add(new Marque(4893353, "False", 1246974, "", 4, "UV", "NISSAN", "", "Y", firstDate, 4));
+        marqueRows.add(new Marque(4893557, "False", 1246974, "", 2, "UV", "SUBARU", "", "Y", firstDate, 4));
+        marqueRows.add(new Marque(4894018, "False", 1246974, "", 2, "UV", "MAZDA", "", "Y", firstDate, 4));
+        marqueRows.add(new Marque(4895203, "False", 1246974, "", 2, "UV", "ROVER", "", "Y", firstDate, 4));
+        marqueRows.add(new Marque(4896300, "False", 1246974, null, 2, "UV", "HYUNDAI", null, "Y", firstDate, 1));
+        marqueRows.add(new Marque(4899527, "False", 1246974, "", 1, "UV", "JEEP", "", "Y", firstDate, 3));
+        marqueRows.add(new Marque(7659280, "False", 1246972, "Y", 3, "", "DAIHATSU", "", "Y", firstDate, 4));
+        marqueRows.add(new Marque(7681544, "False", 1246974, "", 2, "UV", "LANDROVER", "", "Y", firstDate, 4));
+        marqueRows.add(new Marque(7730022, "False", 1246974, "", 2, "UV", "VOLVO", "", "Y", firstDate, 4));
+        marqueRows.add(new Marque(8376505, "False", 1246974, "", 0, "", "ISUZU", "", "Y", firstDate, 4));
+        marqueRows.add(new Marque(8587147, "False", 1246974, "", 0, "", "DAEWOO", "", "Y", firstDate, 4));
+        marqueRows.add(new Marque(9971178, "False", 1246974, "", 1, "", "CHRYSLER", "", "Y", firstDate, 4));
+        marqueRows.add(new Marque(13224369, "False", 1246974, "", 0, "", "VW", "", "Y", secondDate, 4));
+        marqueRows.add(new Marque(6664478, "False", 1246974, "", 0, "", "BMW", "", "Y", secondDate, 4));
+        marqueRows.add(new Marque(1, "False", 1246974, "", 0, "", "TOYOTA", "", "Y", firstDate, 1));
+        marqueRows.add(new Marque(2, "False", 1246974, "", 0, "", "HUMMER", "", "Y", secondDate, 3));
+
+        marques.insert(marqueRows);
+        DBTable.setPrintSQLBeforeExecuting(true);
+        myDatabase.setPrintSQLBeforeExecuting(true);
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        myDatabase.setPrintSQLBeforeExecuting(false);
+        myDatabase.dropTable(myMarqueRow);
+        myDatabase.dropTable(myCarCompanyRow);
+    }
+}
