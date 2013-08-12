43cc029d88293a3b8789d01b2af8572df65370f0
gregorydgraham <gregorydgraham@bdf2cdf3-4205-4e92-8bbf-280280a2a4f9>
2013-08-12 01:07:55 +0000

    [maven-release-plugin]  copy for tag dbvolution-0.7.15

---
+++ tags/dbvolution-0.7.15/src/main/java/nz/co/gregs/dbvolution/databases/H2DB.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2013 greg.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nz.co.gregs.dbvolution.databases;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author greg
+ */
+public class H2DB extends DBDatabase {
+
+    String dateFormatStr = "yyyy-M-d hh:mm:ss";
+    String h2DateFormatStr = "yyyy-M-d HH:mm:ss";
+    SimpleDateFormat strToDateFormat = new SimpleDateFormat(dateFormatStr);
+
+    public H2DB(String jdbcURL, String username, String password) {
+        super("org.h2.Driver", jdbcURL, username, password);
+    }
+
+    @Override
+    public String getDateFormattedForQuery(Date date) {
+        if (date == null) {
+            return getNull();
+        }
+//        yyyy-MM-dd hh:mm:ss[.nnnnnnnnn]
+        return " PARSEDATETIME('" + strToDateFormat.format(date) + "','" + h2DateFormatStr + "') ";
+        //return "'"+strToDateFormat.format(date)+"'";
+    }
+
+    @Override
+    public String formatTableName(String tableName) {
+        return tableName.toUpperCase();
+    }
+
+    @Override
+    public String formatColumnName(String columnName) {
+        return columnName.toUpperCase();
+    }
+    /**
+     *
+     * overrides standard method and uppercases everything
+     *
+     * @param tableName
+     * @param columnName
+     * @return
+     */
+//    @Override
+//    public String formatTableAndColumnName(String tableName, String columnName) {
+//        return super.formatTableAndColumnName(tableName, columnName).toUpperCase();
+//    }
+}
---
+++ tags/dbvolution-0.7.15/src/test/java/nz/co/gregs/dbvolution/h2/DBTableUpdateTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2013 gregory.graham.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nz.co.gregs.dbvolution.h2;
+
+import java.sql.SQLException;
+import nz.co.gregs.dbvolution.UnexpectedNumberOfRowsException;
+import nz.co.gregs.dbvolution.example.Marque;
+import org.junit.Assert;
+import org.junit.Test;
+import static org.hamcrest.Matchers.*;
+
+public class DBTableUpdateTest extends AbstractTest {
+
+    @Test
+    public void changingPrimaryKey() throws SQLException, UnexpectedNumberOfRowsException {
+        Marque marqueExample = new Marque();
+        marqueExample.getUidMarque().isLiterally(1);
+
+        marques.getRowsByExample(marqueExample);
+        marques.print();
+        Marque toyota = marques.getOnlyRowByExample(marqueExample);
+        toyota.uidMarque.isLiterally(99999);
+        Assert.assertThat(marques.getSQLForUpdate(toyota),is("UPDATE MARQUE SET UID_MARQUE = 99999 WHERE UID_MARQUE = 1;"));
+        marques.update(toyota);
+        toyota.name.isLiterally("NOTOYOTA");
+        Assert.assertThat(marques.getSQLForUpdate(toyota), is("UPDATE MARQUE SET NAME = 'NOTOYOTA' WHERE UID_MARQUE = 99999;"));
+        
+        marqueExample = new Marque();
+        marqueExample.name.isLike("toyota");
+        toyota = marques.getOnlyRowByExample(marqueExample);
+        Assert.assertThat(toyota.name.toString(), is("TOYOTA"));
+    }
+    
+    @Test
+    public void testInsertRows() throws SQLException {
+        Marque myTableRow = new Marque();
+        myTableRow.getUidMarque().isLiterally(1);
+
+        marques.getRowsByExample(myTableRow);
+        marques.print();
+        Marque toyota = marques.toList().get(0);
+        System.out.println("===" + toyota.name.toString());
+        Assert.assertEquals("The row retrieved should be TOYOTA", "TOYOTA", toyota.name.toString());
+
+        toyota.name.isLiterally("NOTTOYOTA");
+        String sqlForUpdate = marques.getSQLForUpdate(toyota);
+        Assert.assertEquals("Update statement doesn't look right:", "UPDATE MARQUE SET NAME = 'NOTTOYOTA' WHERE UID_MARQUE = 1;", sqlForUpdate);
+
+        marques.update(toyota);
+        
+        marques.getRowsByExample(myTableRow);
+        marques.print();
+        toyota = marques.toList().get(0);
+        Assert.assertEquals("The row retrieved should be NOTTOYOTA", "NOTTOYOTA", toyota.name.toString());
+    }
+}
