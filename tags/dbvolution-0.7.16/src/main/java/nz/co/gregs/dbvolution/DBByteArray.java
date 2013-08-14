fe68512b168cff864e31982da8bf9b3bf2600644
gregorydgraham <gregorydgraham@bdf2cdf3-4205-4e92-8bbf-280280a2a4f9>
2013-08-14 01:02:40 +0000

    [maven-release-plugin]  copy for tag dbvolution-0.7.16

---
+++ tags/dbvolution-0.7.16/src/main/java/nz/co/gregs/dbvolution/DBByteArray.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nz.co.gregs.dbvolution;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * 
+ * Implements the abstractions necessary to handle arbitrary byte streams and files stored in the database
+ * 
+ * @author gregory.graham
+ */
+public class DBByteArray extends QueryableDatatype {
+
+    public static final long serialVersionUID = 1;
+
+    public DBByteArray(Object object) {
+        super(object);
+
+    }
+
+    public DBByteArray() {
+        super();
+    }
+
+    @Override
+    public String getSQLDatatype() {
+        return "BLOB";
+    }
+    
+    @Override
+        protected void setFromResultSet(ResultSet resultSet, String fullColumnName) throws SQLException{
+        this.isLiterally(resultSet.getBytes(fullColumnName));
+    }
+
+    @Override
+    public String getSQLValue() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
