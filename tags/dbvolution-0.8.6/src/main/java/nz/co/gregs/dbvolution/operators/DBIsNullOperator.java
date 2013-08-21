cf9053f8a6abdf4b8ae245b31a7646e035210b26
gregorydgraham <gregorydgraham@bdf2cdf3-4205-4e92-8bbf-280280a2a4f9>
2013-08-21 12:22:35 +0000

    [maven-release-plugin]  copy for tag dbvolution-0.8.6

---
+++ tags/dbvolution-0.8.6/src/main/java/nz/co/gregs/dbvolution/operators/DBIsNullOperator.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2013 gregorygraham.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nz.co.gregs.dbvolution.operators;
+
+import nz.co.gregs.dbvolution.databases.DBDatabase;
+/**
+ *
+ * @author gregorygraham
+ */
+public class DBIsNullOperator extends DBOperator {
+    public static final long serialVersionUID = 1L;
+
+    @Override
+    public String generateWhereLine(DBDatabase database, String columnName) {
+        return database.beginAndLine() + columnName + (invertOperator ? getInverse() : getOperator());
+    }
+
+    private String getOperator() {
+        return " is null ";
+    }
+    
+    private String getInverse() {
+        return " is not null ";
+    }
+
+    @Override
+    public String generateRelationship(DBDatabase database, String columnName, String otherColumnName) {
+        throw new UnsupportedOperationException("The DB IS NULL Operator Cannot Be Used To Specify a Relationship"); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ tags/dbvolution-0.8.6/src/test/java/nz/co/gregs/dbvolution/h2/PrimaryKeyLessTablesTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2013 gregorygraham.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nz.co.gregs.dbvolution.h2;
+
+import java.sql.SQLException;
+import nz.co.gregs.dbvolution.DBQuery;
+import nz.co.gregs.dbvolution.example.CarCompany;
+import nz.co.gregs.dbvolution.example.CompanyLogo;
+import nz.co.gregs.dbvolution.example.LinkCarCompanyAndLogo;
+import nz.co.gregs.dbvolution.example.Marque;
+import nz.co.gregs.dbvolution.exceptions.AccidentalCartesianJoinException;
+import org.junit.Test;
+
+/**
+ *
+ * @author gregorygraham
+ */
+public class PrimaryKeyLessTablesTest extends AbstractTest {
+
+    public void SetUp() throws SQLException {
+        myDatabase.dropTableNoExceptions(new LinkCarCompanyAndLogo());
+        myDatabase.createTable(new LinkCarCompanyAndLogo());
+    }
+
+    public void TearDown() throws SQLException {
+        myDatabase.dropTable(new LinkCarCompanyAndLogo());
+    }
+
+    @Test
+    public void linkIntoTableTest() throws SQLException {
+        CarCompany carCompany = new CarCompany();
+        LinkCarCompanyAndLogo link = new LinkCarCompanyAndLogo();
+        DBQuery dbQuery = myDatabase.getDBQuery(carCompany, link);
+        dbQuery.print();
+        dbQuery = myDatabase.getDBQuery(new CompanyLogo(), link);
+        dbQuery.print();
+    }
+
+    @Test
+    public void linkThruTableTest() throws SQLException {
+        CarCompany carCompany = new CarCompany();
+        LinkCarCompanyAndLogo link = new LinkCarCompanyAndLogo();
+
+        myDatabase.createTable(new CompanyLogo());
+
+        DBQuery dbQuery = myDatabase.getDBQuery(carCompany, link, new CompanyLogo());
+        dbQuery.print();
+    }
+
+    @Test
+    public void testCartesianJoinProtection() throws SQLException, Exception {
+        try {
+            myDatabase.getDBQuery(new Marque(), new CompanyLogo()).print();
+            throw new Exception("Should have thrown an AccidentalCartesianJoinException here.");
+        } catch (AccidentalCartesianJoinException ex) {
+        }
+    }
+}
