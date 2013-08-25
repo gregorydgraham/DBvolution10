de2c3492adc8d081417ffa68b798d4f9f61ed2bc
gregorydgraham <gregorydgraham@bdf2cdf3-4205-4e92-8bbf-280280a2a4f9>
2013-08-25 10:48:45 +0000

    [maven-release-plugin]  copy for tag dbvolution-0.8.8

---
+++ tags/dbvolution-0.8.8/src/main/java/nz/co/gregs/dbvolution/example/CarCompany.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2013 gregorygraham.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nz.co.gregs.dbvolution.example;
+
+import nz.co.gregs.dbvolution.*;
+import nz.co.gregs.dbvolution.annotations.*;
+
+/**
+ *
+ * @author gregorygraham
+ */
+@DBTableName("car_company")
+public class CarCompany extends DBRow {
+
+    public static final long serialVersionUID = 1L;
+
+    @DBColumn("name")
+    public DBString name = new DBString();
+    @DBPrimaryKey
+    @DBColumn("uid_carcompany")
+    public DBInteger uidCarCompany = new DBInteger();
+
+    public CarCompany() {
+    }
+
+    public CarCompany(String anme, int id) {
+        this.name.setValue(anme);
+        this.uidCarCompany.setValue(id);
+    }
+}
---
+++ tags/dbvolution-0.8.8/src/test/java/nz/co/gregs/dbvolution/h2/PrimaryKeyLessTablesTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2013 gregorygraham.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nz.co.gregs.dbvolution.h2;
+
+import java.sql.SQLException;
+import nz.co.gregs.dbvolution.DBQuery;
+import nz.co.gregs.dbvolution.example.CarCompany;
+import nz.co.gregs.dbvolution.example.CompanyLogo;
+import nz.co.gregs.dbvolution.example.LinkCarCompanyAndLogo;
+import nz.co.gregs.dbvolution.example.Marque;
+import nz.co.gregs.dbvolution.exceptions.AccidentalCartesianJoinException;
+import org.junit.Test;
+
+/**
+ *
+ * @author gregorygraham
+ */
+public class PrimaryKeyLessTablesTest extends AbstractTest {
+
+    @Test
+    public void linkIntoTableTest() throws SQLException {
+        CarCompany carCompany = new CarCompany();
+        LinkCarCompanyAndLogo link = new LinkCarCompanyAndLogo();
+        DBQuery dbQuery = myDatabase.getDBQuery(carCompany, link);
+        dbQuery.print();
+        dbQuery = myDatabase.getDBQuery(new CompanyLogo(), link);
+        dbQuery.print();
+    }
+
+    @Test
+    public void linkThruTableTest() throws SQLException {
+        CarCompany carCompany = new CarCompany();
+        LinkCarCompanyAndLogo link = new LinkCarCompanyAndLogo();
+
+        DBQuery dbQuery = myDatabase.getDBQuery(carCompany, link, new CompanyLogo());
+        dbQuery.print();
+    }
+
+    @Test
+    public void testCartesianJoinProtection() throws SQLException, Exception {
+        try {
+            myDatabase.getDBQuery(new Marque(), new CompanyLogo()).print();
+            throw new Exception("Should have thrown an AccidentalCartesianJoinException here.");
+        } catch (AccidentalCartesianJoinException ex) {
+        }
+    }
+
+    @Test
+    public void testAdHocRelations() throws SQLException, Exception {
+        final CompanyLogo companyLogo = new CompanyLogo();
+        myMarqueRow.addRelationship(myMarqueRow.carCompany, companyLogo,companyLogo.carCompany);
+        myDatabase.getDBQuery(myMarqueRow, companyLogo).print();
+    }
+}
