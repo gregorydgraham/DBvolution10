3d449ad8d9695a04d0d3a32f2b9cb2ecd3f6ef50
toaomalkster <toaomalkster@bdf2cdf3-4205-4e92-8bbf-280280a2a4f9>
2013-09-26 11:12:28 +0000

    Added a "@DBForeignKey(...)" factory method to ParsedAnnotation. This also supports specification of referenced field, which is not actually supported by DBvolution core. Adding this in is a convenient way of presenting to the end-user that their database schema includes something not supported by DBvolution: it'll generate code that has a compiler error.

--- branch/2013-07_EclipseASTCodeGeneration/src/main/java/nz/co/gregs/dbvolution/generation/ast/ParsedAnnotation.java
+++ branch/2013-07_EclipseASTCodeGeneration/src/main/java/nz/co/gregs/dbvolution/generation/ast/ParsedAnnotation.java
@@ -3,6 +3,7 @@ package nz.co.gregs.dbvolution.generation.ast;
 import java.util.List;
 
 import nz.co.gregs.dbvolution.annotations.DBColumn;
+import nz.co.gregs.dbvolution.annotations.DBForeignKey;
 import nz.co.gregs.dbvolution.annotations.DBPrimaryKey;
 import nz.co.gregs.dbvolution.annotations.DBTableName;
 
@@ -12,10 +13,10 @@ import org.eclipse.jdt.core.dom.Annotation;
 import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.MarkerAnnotation;
 import org.eclipse.jdt.core.dom.MemberValuePair;
-import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.NormalAnnotation;
 import org.eclipse.jdt.core.dom.SingleMemberAnnotation;
 import org.eclipse.jdt.core.dom.StringLiteral;
+import org.eclipse.jdt.core.dom.TypeLiteral;
 
 /**
  * Represents an annotation in a source file associated with any
@@ -89,6 +90,65 @@ public class ParsedAnnotation {
 		
 		MarkerAnnotation annotation = ast.newMarkerAnnotation();
 		annotation.setTypeName(typeContext.declarableTypeNameOf(DBPrimaryKey.class, true));
+		
+		return new ParsedAnnotation(typeContext, annotation);
+	}
+
+	/**
+	 * Creates a new annotation instance to mark a database column
+	 * as a foreign key.
+	 * The annotation is not added to the AST and must be added by the caller;
+	 * however any referenced types are added to the imports section of the 
+	 * type context.
+	 * 
+	 * <p> Note: this method supports foreign keys to non-primary columns
+	 * on target tables. At present the actual DBvolution annotation doesn't
+	 * support this. It is included here for the rare cases where a database
+	 * uses such an FK. It wil cause a compiler error in the generated code
+	 * which will allow the end-user to discover the problem easily.
+	 * @param typeContext
+	 * @param referencedClass class mapped to the referenced table
+	 * @param referencedField optional, field maped to the referenced column,
+	 *        a {@code null} value implies defaulting to the primary key on the target table
+	 * @return the newly constructed instance
+	 */
+	public static ParsedAnnotation newDBForeignKeyInstance(ParsedTypeContext typeContext,
+			ParsedClass referencedClass, ParsedField referencedField) {
+		AST ast = typeContext.getAST();
+		Annotation annotation;
+		
+		// annotation with default value only
+		if (referencedField == null) {
+			SingleMemberAnnotation smAnnotation = ast.newSingleMemberAnnotation();
+			smAnnotation.setTypeName(typeContext.declarableTypeNameOf(DBForeignKey.class, true));
+			annotation = smAnnotation;
+
+			TypeLiteral value = ast.newTypeLiteral();
+			value.setType(ast.newSimpleType(ast.newSimpleName(referencedClass.getDeclaredName())));
+			smAnnotation.setValue(value);
+		}
+		
+		// annotation with two values
+		else {
+			NormalAnnotation normalAnnotation = ast.newNormalAnnotation();
+			normalAnnotation.setTypeName(typeContext.declarableTypeNameOf(DBForeignKey.class, true));
+			annotation = normalAnnotation;
+			
+			MemberValuePair pair1 = ast.newMemberValuePair();
+			TypeLiteral value1 = ast.newTypeLiteral();
+			value1.setType(ast.newSimpleType(ast.newSimpleName(referencedClass.getDeclaredName())));
+			pair1.setName(ast.newSimpleName("value"));
+			pair1.setValue(value1);
+			
+			MemberValuePair pair2 = ast.newMemberValuePair();
+			StringLiteral value2 = ast.newStringLiteral();
+			value2.setEscapedValue(referencedField.getName());
+			pair2.setName(ast.newSimpleName("value"));
+			pair2.setValue(value2);
+			
+			normalAnnotation.values().add(pair1);
+			normalAnnotation.values().add(pair2);
+		}
 		
 		return new ParsedAnnotation(typeContext, annotation);
 	}
