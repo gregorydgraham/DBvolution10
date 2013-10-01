a211b2de45616bfef7fff8bfd03bd643b1f25eae
gregorydgraham <gregorydgraham@bdf2cdf3-4205-4e92-8bbf-280280a2a4f9>
2013-10-01 08:41:51 +0000

    [maven-release-plugin]  copy for tag dbvolution-0.8.17

---
+++ tags/dbvolution-0.8.17/src/main/java/nz/co/gregs/dbvolution/actions/DBAction.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2013 gregorygraham.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nz.co.gregs.dbvolution.actions;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author gregorygraham
+ */
+public abstract class DBAction {
+    protected String sql = "";
+    
+    public DBAction() {
+        super();
+    }
+    
+    public DBAction(String sql) {
+        this.sql = sql;
+    }
+    
+    public String getSQLRepresentation(){
+        return sql;
+    }
+
+    public abstract boolean canBeBatched();
+
+    public abstract void execute(Statement statement) throws SQLException ;
+    
+}
