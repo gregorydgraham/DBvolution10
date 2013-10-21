31f8c8830e63c3ad5bd7e74159a423940e7366ae
gregorydgraham <gregorydgraham@bdf2cdf3-4205-4e92-8bbf-280280a2a4f9>
2013-10-21 10:41:01 +0000

    Grumpy pickiness.

--- trunk/src/main/java/nz/co/gregs/dbvolution/annotations/DBAdaptType.java
+++ trunk/src/main/java/nz/co/gregs/dbvolution/annotations/DBAdaptType.java
@@ -10,32 +10,35 @@ import nz.co.gregs.dbvolution.datatypes.QueryableDatatype;
 
 /**
  * Examples:
- * @DBAdaptType(adaptor=DaysSinceEpocDateAdaptor.class, type=DBDate.class)
- * public DBInteger daysSinceEpoc;
- * 
+ *
+ * @DBAdaptType(adaptor=DaysSinceEpochDateAdaptor.class, type=DBDate.class)
+ * public DBInteger daysSinceEpoch;
+ *
  * @DBAdaptType(adaptor=MyFreeTextNumberAdaptor.class, type=DBInteger.class)
  * public String freeTextNumber;
- * 
- * @DBAdaptType(adaptor=TrimmingStringAdaptor.class, type=DBString.class)
- * public DBString trimmedValue;
+ *
+ * @DBAdaptType(adaptor=TrimmingStringAdaptor.class, type=DBString.class) public
+ * DBString trimmedValue;
  */
 @Target({ElementType.FIELD, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface DBAdaptType {
-	/**
-	 * The custom type adaptor to use to convert between
-	 * the type of the annotated field/property and the value of {@link #type()}.
-	 * 
-	 * <p> The indicated class must be instantiable (ie: it cannot be an interface or an abstract class), and
-	 * must have a default constructor.
-	 * @return
-	 */
-    Class<? extends DBTypeAdaptor<?,?>> adaptor();
-    
+
+    /**
+     * The custom type adaptor to use to convert between the type of the
+     * annotated field/property and the value of {@link #type()}.
+     *
+     * <p> The indicated class must be able to be instantiated. It cannot be an interface or an abstract class), and must have a default
+     * constructor.
+     *
+     * @return
+     */
+    Class<? extends DBTypeAdaptor<?, ?>> adaptor();
+
     /**
      * The DBvolution type that the adaptor converts to.
+     *
      * @return
      */
     Class<? extends QueryableDatatype> type() default QueryableDatatype.class;
-    
 }
