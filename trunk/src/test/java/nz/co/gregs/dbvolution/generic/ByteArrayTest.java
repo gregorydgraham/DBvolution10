e78368b598b162c5065055709ed974e70f38f900
gregorydgraham <gregorydgraham@bdf2cdf3-4205-4e92-8bbf-280280a2a4f9>
2013-10-28 22:23:42 +0000

    Removed redundant variables in DBByteArray

--- trunk/src/test/java/nz/co/gregs/dbvolution/generic/ByteArrayTest.java
+++ trunk/src/test/java/nz/co/gregs/dbvolution/generic/ByteArrayTest.java
@@ -43,7 +43,7 @@ public class ByteArrayTest extends AbstractTest {
         companyLogo.logoID.setValue(1);
         companyLogo.carCompany.setValue(1);//Toyota
         companyLogo.imageFilename.setValue("toyota_logo.jpg");
-        companyLogo.imageBytes.readFromFileSystem("toyota_share_logo.jpg");
+        companyLogo.imageBytes.setFromFileSystem("toyota_share_logo.jpg");
         database.insert(companyLogo);
         
         CarCompany carCompany = new CarCompany();
@@ -53,7 +53,7 @@ public class ByteArrayTest extends AbstractTest {
         companyLogo.logoID.setValue(2);
         companyLogo.carCompany.setValue(ford.uidCarCompany.longValue());
         companyLogo.imageFilename.setValue("ford_logo.jpg");
-        companyLogo.imageBytes.readFromFileSystem("ford_logo.jpg");
+        companyLogo.imageBytes.setFromFileSystem("ford_logo.jpg");
         database.insert(companyLogo);
     }
 
@@ -65,7 +65,7 @@ public class ByteArrayTest extends AbstractTest {
         companyLogo.carCompany.setValue(1);//Toyota
         companyLogo.imageFilename.setValue("toyota_logo.jpg");
         File image = new File("toyota_share_logo.jpg");
-        companyLogo.imageBytes.readFromFileSystem(image);
+        companyLogo.imageBytes.setFromFileSystem(image);
         database.insert(companyLogo);
 
         File newFile = new File("found_toyota_logo.jpg");
