e5e8ba90a6363da70ad22c8db2a94dfcf0b022f4
gregorydgraham <gregorydgraham@bdf2cdf3-4205-4e92-8bbf-280280a2a4f9>
2014-01-06 08:07:48 +0000

    Changed the DBUnknownJavaSQLType annotation to use an int and to have the correct sargets

--- src/main/java/nz/co/gregs/dbvolution/annotations/DBUnknownJavaSQLType.java
+++ src/main/java/nz/co/gregs/dbvolution/annotations/DBUnknownJavaSQLType.java
@@ -16,13 +16,21 @@
 package nz.co.gregs.dbvolution.annotations;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
  *
  * @author gregorygraham
  */
-@Target(ElementType.TYPE)
+@Target({ElementType.FIELD, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
 public  @interface DBUnknownJavaSQLType {
-    String value();
+
+    /**
+     *
+     * @return
+     */
+    int value();
 }
