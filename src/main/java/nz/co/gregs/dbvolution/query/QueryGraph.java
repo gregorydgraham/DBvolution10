1bc9313a80dc9e69956e5b1e8fe1d76ac5f794c3
gregorydgraham <gregorydgraham@bdf2cdf3-4205-4e92-8bbf-280280a2a4f9>
2014-02-13 06:26:37 +0000

    QueryGraph simplified

--- src/main/java/nz/co/gregs/dbvolution/query/QueryGraph.java
+++ src/main/java/nz/co/gregs/dbvolution/query/QueryGraph.java
@@ -16,7 +16,6 @@
 package nz.co.gregs.dbvolution.query;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
@@ -39,26 +38,6 @@ public class QueryGraph {
 
     public QueryGraph(DBDatabase database, List<DBRow> allQueryTables, QueryOptions options) {
         addAndConnectToRelevant(database, allQueryTables, options);
-    }
-
-    private QueryGraph() {
-    }
-
-    private void addAndConnect(Class<? extends DBRow> table1, Class<? extends DBRow> table2) {
-        QueryGraphNode node1 = nodes.get(table1);
-        if (node1 == null) {
-            node1 = new QueryGraphNode(table1);
-            nodes.put(table1, node1);
-        }
-
-        QueryGraphNode node2 = nodes.get(table2);
-        if (node2 == null) {
-            node2 = new QueryGraphNode(table2);
-            nodes.put(table2, node2);
-        }
-        node1.connectTable(table2);
-        node2.connectTable(table1);
-
     }
 
     private void addAndConnectToRelevant(DBDatabase database, List<DBRow> otherTables, QueryOptions options) {
@@ -96,61 +75,17 @@ public class QueryGraph {
         }
     }
 
-    public boolean hasDisconnectedSubgraph() {
-        boolean disconnected = false;
-        QueryGraph fullGraph = new QueryGraph();
-        synchronized (mutex) {
-            fullGraph.addAll(this);
-        }
-        if (!fullGraph.nodes.isEmpty()) {
-            QueryGraphNode nodeA;
-            QueryGraph testGraph = new QueryGraph();
-            List<QueryGraphNode> added;
-            List<QueryGraphNode> addedNext;
-            int previousSize;
+    public boolean willCreateCartesianJoin() { //willCreateCartesianJoin
+        Set<DBRow> returnTables = new HashSet<DBRow>();
+        Class<? extends DBRow> startFrom = nodes.keySet().iterator().next();
+        returnTables.addAll(toList(startFrom));
 
-            nodeA = fullGraph.nodes.values().toArray(new QueryGraphNode[]{})[0];
-            previousSize = testGraph.nodes.size();
-            added = testGraph.addConnected(nodeA, nodes);
-
-            while (testGraph.nodes.size() > previousSize) {
-                previousSize = testGraph.nodes.size();
-                addedNext = new ArrayList<QueryGraphNode>();
-                for (QueryGraphNode node : added) {
-                    addedNext.addAll(testGraph.addConnected(node, nodes));
-                }
-                added = addedNext;
-            }
-
-            if (testGraph.nodes.size() != fullGraph.nodes.size()) {
-                disconnected = true;
+        for (DBRow row : rows.values()) {
+            if (!returnTables.contains(row)) {
+                return true;
             }
         }
-        return disconnected;
-    }
-
-    private List<QueryGraphNode> addConnected(QueryGraphNode nodeA, Map<Class<? extends DBRow>, QueryGraphNode> nodes) {
-        List<QueryGraphNode> addedTables = new ArrayList<QueryGraphNode>();
-        for (Class<? extends DBRow> table : nodeA.getConnectedTables()) {
-            QueryGraphNode nodeToAdd = nodes.get(table);
-            Class<? extends DBRow> tableToAdd = nodeToAdd.getTable();
-            addAndConnect(nodeA.getTable(), tableToAdd);
-            addedTables.add(nodeToAdd);
-        }
-        return addedTables;
-    }
-
-//    private void add(Class<? extends DBRow> table) {
-//        QueryGraphNode node = nodes.get(table);
-//        if (node == null) {
-//            node = new QueryGraphNode(table);
-//            nodes.put(table, node);
-//        }
-//    }
-    private void addAll(QueryGraph otherGraph) {
-        for (Map.Entry<Class<? extends DBRow>, QueryGraphNode> entry : otherGraph.nodes.entrySet()) {
-            this.nodes.put(entry.getKey(), new QueryGraphNode(entry.getValue()));
-        }
+        return false;
     }
 
     public List<DBRow> toList(Class<? extends DBRow> startFrom) {
@@ -180,7 +115,7 @@ public class QueryGraph {
 
     public List<DBRow> toListIncludingCartesian(Class<? extends DBRow> startFrom) {
         Set<DBRow> returnTables = new HashSet<DBRow>();
-        
+
         returnTables.addAll(toList(startFrom));
         boolean changed = true;
 
