533995c6228ec5a8b495bf94be8df93824603a04
gregorydgraham <gregorydgraham@bdf2cdf3-4205-4e92-8bbf-280280a2a4f9>
2014-03-13 07:59:31 +0000

    [maven-release-plugin]  copy for tag dbvolution-0.9.12

---
+++ tags/dbvolution-0.9.12/trunk/src/test/java/nz/co/gregs/dbvolution/internal/properties/PropertyWrapperDefinitionTest.java
@@ -0,0 +1,78 @@
+package nz.co.gregs.dbvolution.internal.properties;
+
+import nz.co.gregs.dbvolution.internal.properties.DBRowClassWrapper;
+import nz.co.gregs.dbvolution.internal.properties.PropertyWrapperDefinition;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+import nz.co.gregs.dbvolution.DBRow;
+import nz.co.gregs.dbvolution.annotations.DBColumn;
+import nz.co.gregs.dbvolution.annotations.DBTableName;
+import nz.co.gregs.dbvolution.datatypes.DBInteger;
+
+import org.junit.Test;
+
+@SuppressWarnings("serial")
+public class PropertyWrapperDefinitionTest {
+
+    @Test
+    public void dotEqualsTrueWhenDifferentObjectAndSameClass() {
+        class MyClass extends DBRow {
+
+            @DBColumn
+            public DBInteger intField1 = new DBInteger();
+            @DBColumn
+            public DBInteger intField2 = new DBInteger();
+        }
+
+        PropertyWrapperDefinition intField1_obj1 = propertyDefinitionOf(new MyClass(), "intField1");
+        PropertyWrapperDefinition intField1_obj2 = propertyDefinitionOf(new MyClass(), "intField1");
+        assertThat(intField1_obj1 == intField1_obj2, is(false));
+
+        assertThat(intField1_obj1.equals(intField1_obj2), is(true));
+    }
+
+    @Test
+    public void dotEqualsTrueWhenDifferentButIdenticalClass() {
+        class MyClass1 extends DBRow {
+
+            @DBColumn
+            public DBInteger intField1 = new DBInteger();
+            @DBColumn
+            public DBInteger intField2 = new DBInteger();
+        }
+
+        class MyClass2 extends DBRow {
+
+            @DBColumn
+            public DBInteger intField1 = new DBInteger();
+            @DBColumn
+            public DBInteger intField2 = new DBInteger();
+        }
+
+        PropertyWrapperDefinition intField1_obj1 = propertyDefinitionOf(new MyClass1(), "intField1");
+        PropertyWrapperDefinition intField1_obj2 = propertyDefinitionOf(new MyClass2(), "intField1");
+        assertThat(intField1_obj1.equals(intField1_obj2), is(false));
+    }
+
+	@Test
+	public void getsTableNameViaProperty() {
+		@DBTableName("Customer")
+		class MyClass extends DBRow {
+			@DBColumn
+			public DBInteger intField1 = new DBInteger();
+		}
+		
+		PropertyWrapperDefinition property = propertyDefinitionOf(new MyClass(), "intField1");
+		assertThat(property.tableName(), is("Customer"));
+	}
+	
+    private PropertyWrapperDefinition propertyDefinitionOf(Object target, String javaPropertyName) {
+        return propertyDefinitionOf(target.getClass(), javaPropertyName);
+    }
+
+    // note: intentionally doesn't use a wrapper factory for tests on equals() methods
+    private PropertyWrapperDefinition propertyDefinitionOf(Class<?> clazz, String javaPropertyName) {
+        DBRowClassWrapper classWrapper = new DBRowClassWrapper(clazz);
+        return classWrapper.getPropertyDefinitionByName(javaPropertyName);
+    }
+}
