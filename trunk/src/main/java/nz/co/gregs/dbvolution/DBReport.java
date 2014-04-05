974d166eadc31d3ee601e54cc5595e25c8cf4dcc
gregorydgraham <gregorydgraham@bdf2cdf3-4205-4e92-8bbf-280280a2a4f9>
2014-04-05 08:35:28 +0000

    JavaDocs

--- trunk/src/main/java/nz/co/gregs/dbvolution/DBReport.java
+++ trunk/src/main/java/nz/co/gregs/dbvolution/DBReport.java
@@ -137,27 +137,116 @@ public class DBReport extends RowDefinition {
 		return reportRows;
 	}
 
+	/**
+	 * Generates and returns the actual SQL to be used by this DBReport.
+	 *
+	 * <p>
+	 * Good for debugging and great for DBAs, this is how you find out what
+	 * DBvolution is really doing.
+	 *
+	 * <p>
+	 * Generates the SQL query for retrieving the objects but does not execute
+	 * the SQL. Use {@link #getAllRows(nz.co.gregs.dbvolution.DBDatabase, nz.co.gregs.dbvolution.DBReport)  the getAllRows method} to retrieve the
+	 * rows.
+	 *
+	 * <p>
+	 * See also
+	 * {@link #getSQLForCount(nz.co.gregs.dbvolution.DBDatabase, nz.co.gregs.dbvolution.DBReport, nz.co.gregs.dbvolution.DBRow...)}
+	 *
+	 * @param <A> the class of the supplied report.
+	 * @param database the database the SQL will be run against.
+	 * @param exampleReport the report required.
+	 * @param rows additional conditions to apply to the report.
+	 * @return a String of the SQL that will be used by this DBQuery.
+	 * @throws SQLException
+	 */
 	public static <A extends DBReport> String getSQLForQuery(DBDatabase database, A exampleReport, DBRow... rows) throws SQLException {
 		DBQuery query = setUpQuery(database, exampleReport, rows);
 		return query.getSQLForQuery();
 	}
 
-	public static <A extends DBReport> String getSQLForCount(DBDatabase database, A exampleReport, DBRow... rows) throws SQLException {
+	/**
+	 * Returns the SQL query that will used to count the rows returned for the
+	 * supplied DBReport
+	 *
+	 * <p>
+	 * Use this method to check the SQL that will be executed during
+	 * {@link DBReport#count(nz.co.gregs.dbvolution.DBDatabase, nz.co.gregs.dbvolution.DBReport, nz.co.gregs.dbvolution.DBRow...)  the count method}
+	 *
+	 * @param database the database to format the query for.
+	 * @param exampleReport the report to retrieve.
+	 * @param rows additional conditions to be applied.
+	 * @return a String of the SQL query that will be used to count the rows
+	 * returned by this report
+	 * @throws SQLException
+	 */
+	public static String getSQLForCount(DBDatabase database, DBReport exampleReport, DBRow... rows) throws SQLException {
 		DBQuery query = setUpQuery(database, exampleReport, rows);
 		return query.getSQLForCount();
 	}
 
-	public void setSortOrder(ColumnProvider... columns) {
-		sortColumns = new ColumnProvider[columns.length];
-		System.arraycopy(columns, 0, sortColumns, 0, columns.length);
+	/**
+	 * Count the rows on the database without retrieving the rows.
+	 *
+	 * <p>
+	 * Creates a {@link #getSQLForCount(nz.co.gregs.dbvolution.DBDatabase, nz.co.gregs.dbvolution.DBReport, nz.co.gregs.dbvolution.DBRow...)  count query} for the report and
+	 * conditions and retrieves the number of rows that would have been returned
+	 * had {@link #getAllRows(nz.co.gregs.dbvolution.DBDatabase, nz.co.gregs.dbvolution.DBReport)  getAllRows method} been called.
+	 *
+	 * @param database the database to format the query for.
+	 * @param exampleReport the report required.
+	 * @param rows additional conditions for the query.
+	 * @return the number of rows that have or will be retrieved.
+	 * @throws SQLException
+	 */
+	public static Long count(DBDatabase database, DBReport exampleReport, DBRow... rows) throws SQLException {
+		DBQuery setUpQuery = setUpQuery(database, exampleReport, rows);
+		return setUpQuery.count();
 	}
 
-	public void setSortOrder(QueryableDatatype... columns) {
+	/**
+	 * Sets the sort order of DBReport (field and/or method) by the given column
+	 * providers.
+	 *
+	 * <p>
+	 * For example the following code snippet will sort by just the name column:
+	 * <pre>
+	 * CustomerReport customers = ...;
+	 * customers.setSortOrder(customers.column(customers.name));
+	 * </pre>
+	 *
+	 * @param columns a list of columns to sort the query by.
+	 * @return this DBReport instance
+	 */
+	public DBReport setSortOrder(ColumnProvider... columns) {
+		sortColumns = new ColumnProvider[columns.length];
+		System.arraycopy(columns, 0, sortColumns, 0, columns.length);
+		return this;
+	}
+
+	/**
+	 * Sets the sort order of DBReport (field and/or method) by the given column
+	 * providers.
+	 *
+	 * <p>
+	 * ONLY USE FIELDS FROM THE SAME INSTANCE.
+	 * <p>
+	 * For example the following code snippet will sort by just the name column:
+	 * <pre>
+	 * CustomerReport customers = ...;
+	 * customers.setSortOrder(customers.name, customers.accountNumber);
+	 * </pre>
+	 *
+	 * @param columns a list of columns to sort the query by.
+	 * @return this DBReport instance
+	 */
+	public DBReport setSortOrder(QueryableDatatype... columns) {
 		List<ColumnProvider> columnProviders = new ArrayList<ColumnProvider>();
 		for (QueryableDatatype qdt : columns) {
 			columnProviders.add(this.column(qdt));
 		}
 		sortColumns = columnProviders.toArray(new ColumnProvider[]{});
+		return this;
 	}
 
 	private static <A extends DBReport> DBQuery setUpQuery(DBDatabase database, A exampleReport, DBRow[] rows) {
@@ -233,24 +322,7 @@ public class DBReport extends RowDefinition {
 		}
 	}
 
-//    private List<DBRow> getAllDBRowTemplates() {
-//        ArrayList<DBRow> arrayList = new ArrayList<DBRow>();
-//        Field[] fields = this.getClass().getFields();
-//        for (Field field : fields) {
-//            try {
-//                final Object fieldValue = field.get(this);
-//                if (DBRow.class.isAssignableFrom(fieldValue.getClass())) {
-//                    arrayList.add((DBRow) fieldValue);
-//                }
-//            } catch (IllegalArgumentException ex) {
-//                throw new UnableToAccessDBReportFieldException(this, field, ex);
-//            } catch (IllegalAccessException ex) {
-//                throw new UnableToAccessDBReportFieldException(this, field, ex);
-//            }
-//        }
-//        return arrayList;
-//    }
-	private static class UnableToAccessDBReportFieldException extends RuntimeException {
+	public static class UnableToAccessDBReportFieldException extends RuntimeException {
 
 		public static final long serialVersionUID = 1L;
 
@@ -263,7 +335,7 @@ public class DBReport extends RowDefinition {
 		}
 	}
 
-	private static class UnableToCreateDBReportSubclassException extends RuntimeException {
+	public static class UnableToCreateDBReportSubclassException extends RuntimeException {
 
 		public static final long serialVersionUID = 1L;
 
