21d08da5b343f29acac8686ace5d72850a224e2e
gregorydgraham <gregorydgraham@bdf2cdf3-4205-4e92-8bbf-280280a2a4f9>
2014-04-27 11:42:11 +0000

    JavaDocs

--- src/main/java/nz/co/gregs/dbvolution/annotations/DBAdaptType.java
+++ src/main/java/nz/co/gregs/dbvolution/annotations/DBAdaptType.java
@@ -9,16 +9,37 @@ import java.util.Set;
 
 import nz.co.gregs.dbvolution.DBDatabase;
 import nz.co.gregs.dbvolution.DBRow;
+import nz.co.gregs.dbvolution.datatypes.DBInteger;
+import nz.co.gregs.dbvolution.datatypes.DBString;
 import nz.co.gregs.dbvolution.datatypes.DBTypeAdaptor;
 import nz.co.gregs.dbvolution.datatypes.QueryableDatatype;
 
 /**
  * Adapts a non-DBvolution field or property to a DBvolution type, or adapts a
- * DBvolution field or property to a different DBvolution type. Where the
- * adapted field or property is a non-DBvolution type, a null value returned by
- * the type adaptor is translated into a non-null {@link QueryableDatatype} of
- * the appropriate type with {@link QueryableDatatype#isNull()} {@code true} and
- * the field is not used in the {@code WHERE} clause of queries.
+ * DBvolution field or property to a different DBvolution type.
+ *
+ * <p>
+ * DBAdaptType uses a {@link DBTypeAdaptor} and a {@link QueryableDatatype} such
+ * as {@link DBInteger} or {@link DBString} to convert database class into a
+ * different Java class
+ *
+ * <p>
+ * In some databases values are stored in an unusual datatype. For instance a
+ * date might be stored as an integer or a integer as a string. It is possible
+ * to implement these as a custom QueryableDatatype but it is much easier to
+ * "adapt" the value to an existing QDT. This also allows for a form of
+ * pre-processing of values like the TrimmingStringAdaptor example below.
+ *
+ * <p>
+ * Think of the &#64;DAdaptType annotation adding a bridge between the actual
+ * QDT and the perceived QDT. This is similar to the bridge that QDTs provide
+ * between the actual DB value and the perceived Java value.
+ *
+ * <p>
+ * Where the adapted field or property is a non-QDT type, a null value returned
+ * by the type adaptor is translated into a non-null {@link QueryableDatatype}
+ * of the appropriate type with {@link QueryableDatatype#isNull()} {@code true}
+ * and the field is not used in the {@code WHERE} clause of queries.
  *
  * Examples:
  * <pre>
@@ -36,58 +57,58 @@ import nz.co.gregs.dbvolution.datatypes.QueryableDatatype;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface DBAdaptType {
 
-    /**
-     * Indicates that the 'type' is implied by other details.
-     */
-    static class Implicit extends QueryableDatatype {
+	/**
+	 * Indicates that the 'type' is implied by other details.
+	 */
+	static class Implicit extends QueryableDatatype {
 
-        private static final long serialVersionUID = 1L;
+		private static final long serialVersionUID = 1L;
 
-        private Implicit() {
-        }
+		private Implicit() {
+		}
 
-        @Override
-        public String getSQLDatatype() {
-            return null;
-        }
+		@Override
+		public String getSQLDatatype() {
+			return null;
+		}
 
-        @Override
-        protected String formatValueForSQLStatement(DBDatabase db) {
-            return null;
-        }
+		@Override
+		protected String formatValueForSQLStatement(DBDatabase db) {
+			return null;
+		}
 
-        @Override
-        public void setValue(Object newLiteralValue) {
-        }
+		@Override
+		public void setValue(Object newLiteralValue) {
+		}
 
-        @Override
-        public boolean isAggregator() {
-            return false;
-        }
+		@Override
+		public boolean isAggregator() {
+			return false;
+		}
 
-        @Override
-        public Set<DBRow> getTablesInvolved() {
-            return new HashSet<DBRow>();
-        }
-    }
+		@Override
+		public Set<DBRow> getTablesInvolved() {
+			return new HashSet<DBRow>();
+		}
+	}
 
-    /**
-     * The custom type adaptor to use to convert between the type of the
-     * annotated field/property and the value of {@link #type()}.
-     *
-     * <p>
-     * The indicated class must be able to be instantiated. It cannot be an
-     * interface or an abstract class and must have a default constructor.
-     *
-     * @return the adaptor used to mediate between the external java object
-     * (possibly a QueryableDatatype) and the internalQueryableDatatype.
-     */
-    Class<? extends DBTypeAdaptor<?, ?>> value();
+	/**
+	 * The custom type adaptor to use to convert between the type of the annotated
+	 * field/property and the value of {@link #type()}.
+	 *
+	 * <p>
+	 * The indicated class must be able to be instantiated. It cannot be an
+	 * interface or an abstract class and must have a default constructor.
+	 *
+	 * @return the adaptor used to mediate between the external java object
+	 * (possibly a QueryableDatatype) and the internalQueryableDatatype.
+	 */
+	Class<? extends DBTypeAdaptor<?, ?>> value();
 
-    /**
-     * The DBvolution type that the adaptor converts to.
-     *
-     * @return the QueryableDatatype class used internally for DB communication
-     */
-    Class<? extends QueryableDatatype> type() default Implicit.class;
+	/**
+	 * The DBvolution type that the adaptor converts to.
+	 *
+	 * @return the QueryableDatatype class used internally for DB communication
+	 */
+	Class<? extends QueryableDatatype> type() default Implicit.class;
 }
