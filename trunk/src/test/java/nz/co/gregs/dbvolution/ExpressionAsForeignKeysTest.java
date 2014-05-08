d649522ef21a34d197f3e80f9e7163db55de586f
gregorydgraham <gregorydgraham@bdf2cdf3-4205-4e92-8bbf-280280a2a4f9>
2014-05-08 13:24:49 +0000

    First simple test of using expressions as ad-hoc foreign keys

---
+++ trunk/src/test/java/nz/co/gregs/dbvolution/ExpressionAsForeignKeysTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2014 gregorygraham.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nz.co.gregs.dbvolution;
+
+import nz.co.gregs.dbvolution.example.CarCompany;
+import nz.co.gregs.dbvolution.example.Marque;
+import nz.co.gregs.dbvolution.generic.AbstractTest;
+import static org.hamcrest.Matchers.is;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author gregorygraham
+ */
+public class ExpressionAsForeignKeysTest extends AbstractTest {
+
+	public ExpressionAsForeignKeysTest(Object testIterationName, Object db) {
+		super(testIterationName, db);
+	}
+
+	@Test
+	public void testSimpleExpressionsAsForeignKeysDoesntThrowCartesianJoinException() {
+
+		DBQuery dbQuery = database.getDBQuery();
+		CarCompany carCompany = new CarCompany();
+		carCompany.name.permittedValues("TOYOTA");
+		carCompany.ignoreAllForeignKeys();
+		final Marque marque = new Marque();
+		marque.ignoreAllForeignKeys();
+		dbQuery.add(marque);
+		dbQuery.add(carCompany);
+		dbQuery.addCondition(marque.column(marque.carCompany).is(carCompany.column(carCompany.uidCarCompany)));
+        final String generateSQLString = dbQuery.getSQLForQuery();
+
+		String expectedResult = "select __1997432637.numeric_code, __1997432637.uid_marque, __1997432637.isusedfortafros, __1997432637.fk_toystatusclass, __1997432637.intindallocallowed, __1997432637.upd_count, __1997432637.auto_created, __1997432637.name, __1997432637.pricingcodeprefix, __1997432637.reservationsalwd, __1997432637.creation_date, __1997432637.enabled, __1997432637.fk_carcompany, __78874071.name, __78874071.uid_carcompany from marque as __1997432637 inner join car_company as __78874071 on( 1=1 ) where 1=1 and (__78874071.name = 'toyota') and (__1997432637.FK_CARCOMPANY = __78874071.UID_CARCOMPANY) ;";
+
+		System.out.println(expectedResult);
+		System.out.println(generateSQLString);
+		Assert.assertThat(testableSQLWithoutColumnAliases(generateSQLString),
+				is(testableSQLWithoutColumnAliases(expectedResult)));
+//		throw new RuntimeException("TODO");
+	}
+
+}
