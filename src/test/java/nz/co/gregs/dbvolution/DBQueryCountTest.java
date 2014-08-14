4c28deecd36f49d9c13f339e50ce3640a3c5bec2
gregorydgraham <gregorydgraham@bdf2cdf3-4205-4e92-8bbf-280280a2a4f9>
2014-08-14 03:07:31 +0000

    Oracle support at 96.00%

--- src/test/java/nz/co/gregs/dbvolution/DBQueryCountTest.java
+++ src/test/java/nz/co/gregs/dbvolution/DBQueryCountTest.java
@@ -48,7 +48,8 @@ public class DBQueryCountTest extends AbstractTest{
 		String expectedResultUsingONClause = "select count(*) from marque as __1997432637 inner join car_company as __78874071 on( ((__78874071.name = 'toyota')) and (__1997432637.fk_carcompany = __78874071.uid_carcompany) ) ;";
 		String expectedResultUsingWHEREClause   = "select count(*) from marque as __1997432637 inner join car_company as __78874071 on( __1997432637.fk_carcompany = __78874071.uid_carcompany ) where 1=1 and (__78874071.name = 'toyota') ;";
 		String expectedResultUsingWHEREClause2 = "select count(*) from car_company as __78874071 inner join marque as __1997432637 on( __1997432637.fk_carcompany = __78874071.uid_carcompany ) where 1=1 and (__78874071.name = 'toyota') ;";
-		String expectedResultUsingWHEREClauseFromOracle = "select count(*) from car_company as OO78874071 inner join marque as OO1997432637 on( OO1997432637.fk_carcompany = OO78874071.uid_carcompany ) where 1=1 and (OO78874071.name = 'toyota')";
+//		String expectedResultUsingWHEREClauseFromOracle = "select count(*) from car_company as OO78874071 inner join marque as OO1997432637 on( OO1997432637.fk_carcompany = OO78874071.uid_carcompany ) where 1=1 and (OO78874071.name = 'toyota')";
+		String expectedResultUsingWHEREClauseFromOracle = "select count(*) from car_company __78874071 inner join marque __1997432637 on( __1997432637.fk_carcompany = __78874071.uid_carcompany ) where 1=1 and (__78874071.name = 'toyota')";
 		Assert.assertThat(testableSQLWithoutColumnAliases(generateSQLString),
 				anyOf(is(testableSQLWithoutColumnAliases(expectedResultUsingONClause)),
 						is(testableSQLWithoutColumnAliases(expectedResultUsingWHEREClause)),
