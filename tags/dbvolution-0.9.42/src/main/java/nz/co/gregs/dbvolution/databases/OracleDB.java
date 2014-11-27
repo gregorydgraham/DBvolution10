fbd64461008b0f237b3d28bb14f66e86ac7a6ac7
gregorydgraham <gregorydgraham@bdf2cdf3-4205-4e92-8bbf-280280a2a4f9>
2014-11-27 05:18:00 +0000

    [maven-release-plugin]  copy for tag dbvolution-0.9.42

---
+++ tags/dbvolution-0.9.42/src/main/java/nz/co/gregs/dbvolution/databases/OracleDB.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2014 Gregory Graham.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nz.co.gregs.dbvolution.databases;
+
+import javax.sql.DataSource;
+import nz.co.gregs.dbvolution.DBDatabase;
+import nz.co.gregs.dbvolution.databases.definitions.DBDefinition;
+
+/**
+ * Super class for connecting the different versions of the Oracle DB.
+ *
+ * <p>
+ * You should probably use {@link Oracle11DB} or {@link Oracle12DB} instead.
+ *
+ * @author Gregory Graham
+ * @see Oracle11DB
+ * @see Oracle12DB
+ */
+public abstract class OracleDB extends DBDatabase {
+
+	/**
+	 * Creates a DBDatabase instance for the definition and data source.
+	 *
+	 * <p>
+	 * You should probably be using {@link Oracle11DB#Oracle11DB(java.lang.String, int, java.lang.String, java.lang.String, java.lang.String)
+	 * } or
+	 * {@link Oracle12DB#Oracle12DB(java.lang.String, int, java.lang.String, java.lang.String, java.lang.String)}
+	 *
+	 * @param definition
+	 * @param driverName
+	 * @param jdbcURL
+	 * @param username
+	 * @param password
+	 */
+	public OracleDB(DBDefinition definition, String driverName, String jdbcURL, String username, String password) {
+		super(definition, driverName, jdbcURL, username, password);
+	}
+
+	/**
+	 * Creates a DBDatabase instance.
+	 *
+	 * @param dbDefinition an oracle database definition instance
+	 * @param dataSource a data source to an Oracle database
+	 */
+	public OracleDB(DBDefinition dbDefinition, DataSource dataSource) {
+		super(dbDefinition, dataSource);
+	}
+
+	@Override
+	public DBDatabase clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+
+	/**
+	 * Oracle does not differentiate between NULL and an empty string.
+	 *
+	 * @return FALSE.
+	 */
+	@Override
+	public Boolean supportsDifferenceBetweenNullAndEmptyString() {
+		return false;
+	}
+
+}
---
+++ tags/dbvolution-0.9.42/src/main/java/nz/co/gregs/dbvolution/databases/definitions/InformixDBDefinition.java
@@ -0,0 +1,181 @@
+/*
+ * Copyright 2013 Gregory Graham.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nz.co.gregs.dbvolution.databases.definitions;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import nz.co.gregs.dbvolution.DBRow;
+import nz.co.gregs.dbvolution.databases.InformixDB;
+import nz.co.gregs.dbvolution.query.QueryOptions;
+
+/**
+ * Defines the features of the Informix database that differ from the standard
+ * database.
+ *
+ * <p>
+ * This DBDefinition is automatically included in {@link InformixDB} instances, and
+ * you should not need to use it directly.
+ *
+ * @author Gregory Graham
+ */
+public class InformixDBDefinition extends DBDefinition {
+
+    private final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    //TO_DATE("1998-07-07 10:24",   "%Y-%m-%d %H:%M")
+    private final String informixDateFormat = "%Y-%m-%d %H:%M:%S";
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+
+    @Override
+    public boolean prefersIndexBasedGroupByClause() {
+        return true;
+    }
+
+    @Override
+    public boolean prefersIndexBasedOrderByClause() {
+        return true;
+    }
+
+    @Override
+    public String getDateFormattedForQuery(Date date) {
+        return "TO_DATE('" + dateFormat.format(date) + "','" + informixDateFormat + "')";
+    }
+
+    /**
+     *
+     * @param table
+     * @param columnName
+     * @return a string of the table and column name for the select clause
+     */
+    @Override
+    public String formatTableAndColumnName(DBRow table, String columnName) {
+        return "" + formatTableName(table) + "." + formatColumnName(columnName) + "";
+    }
+
+    @Override
+    public Object getLimitRowsSubClauseDuringSelectClause(QueryOptions options) {
+        int rowLimit = options.getRowLimit();
+        Integer pageNumber = options.getPageIndex();
+        if (rowLimit < 1) {
+            return "";
+        } else {
+            if (supportsPagingNatively(options)) {
+                long offset = pageNumber * rowLimit;
+                if (offset > 0L) {
+                    return " SKIP " + offset + " FIRST " + rowLimit + " ";
+                } else {
+                    return " FIRST " + rowLimit + " ";
+                }
+            } else {
+                return " FIRST " + rowLimit + " ";
+            }
+        }
+    }
+
+    @Override
+    public Object getLimitRowsSubClauseAfterWhereClause(QueryOptions options) {
+        return "";
+    }
+
+	@Override
+	protected String getCurrentTimeFunction() {
+		throw new UnsupportedOperationException("Informix Does Not Support CurrentTime as a Function: Please use doCurrentTimeTransform() instead of getCurrentTimeFunction().");
+	}
+
+	@Override
+	protected String getCurrentDateTimeFunction() {
+		throw new UnsupportedOperationException("Informix Does Not Support CurrentDateTime as a Function: Please use doCurrentDateTimeTransform() instead of getCurrentDateTimeFunction().");
+	}
+
+    @Override
+    protected String getCurrentDateOnlyFunctionName() {
+		throw new UnsupportedOperationException("Informix Does Not Support CurrentDateOnly as a Function: Please use doCurrentDateOnlyTransform() instead of getCurrentDateOnlyFunctionName().");
+    }
+
+	@Override
+	public String doCurrentTimeTransform() {
+		return "(CURRENT HOUR TO FRACTION(3))";
+	}
+
+	@Override
+	public String doCurrentDateTimeTransform() {
+		return "(CURRENT)";
+	}
+
+	@Override
+	public String doCurrentDateOnlyTransform() {
+		return "(CURRENT YEAR TO DAY)";
+	}
+
+    @Override
+    public String doDayTransform(String dateExpression) {
+        return "DAY(" + dateExpression + ")";
+    }
+
+    @Override
+    public String doMonthTransform(String dateExpression) {
+        return "MONTH(" + dateExpression + ")";
+    }
+
+    @Override
+    public String doYearTransform(String dateExpression) {
+        return "YEAR(" + dateExpression + ")";
+    }
+
+    @Override
+    public boolean supportsPagingNatively(QueryOptions options) {
+        return false;
+    }
+
+    @Override
+    public boolean supportsGeneratedKeys() {
+        return false;
+    }
+
+	@Override
+	public String doAddYearsTransform(String dateValue, String numberOfYears) {
+		return "(("+dateValue+")+ ("+numberOfYears+") UNITS YEAR)";
+	}
+
+	@Override
+	public String doAddMonthsTransform(String dateValue, String numberOfMonths) {
+		return "(("+dateValue+")+ ("+numberOfMonths+") UNITS MONTH)";
+	}
+
+	@Override
+	public String doAddWeeksTransform(String dateValue, String numberOfWeeks) {
+		return "(("+dateValue+")+ ("+numberOfWeeks+") UNITS WEEK)";
+	}
+
+	@Override
+	public String doAddHoursTransform(String dateValue, String numberOfHours) {
+		return "(("+dateValue+")+ ("+numberOfHours+") UNITS HOUR)";
+	}
+
+	@Override
+	public String doAddDaysTransform(String dateValue, String numberOfDays) {
+		return "(("+dateValue+")+ ("+numberOfDays+") UNITS DAY)";
+	}
+
+	@Override
+	public String doAddMinutesTransform(String dateValue, String numberOfMinutes) {
+		return "(("+dateValue+")+ ("+numberOfMinutes+") UNITS MINUTE)";
+	}
+
+	@Override
+	public String doAddSecondsTransform(String dateValue, String numberOfSeconds) {
+		return "(("+dateValue+")+ ("+numberOfSeconds+") UNITS SECOND)";
+	}
+}
