c8c5ca38bad34d9fce77486fe017c2fea6f0ac1b
gregorydgraham <gregorydgraham@bdf2cdf3-4205-4e92-8bbf-280280a2a4f9>
2015-01-26 21:46:33 +0000

    Added exceptions to make errors in DBEqualsOperator clearer

---
+++ trunk/src/main/java/nz/co/gregs/dbvolution/exceptions/ComparisonBetweenTwoDissimilarTypes.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2015 gregory.graham.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nz.co.gregs.dbvolution.exceptions;
+
+import nz.co.gregs.dbvolution.DBDatabase;
+import nz.co.gregs.dbvolution.expressions.DBExpression;
+
+/**
+ * Thrown when the two data types used in a comparison does not support that
+ * comparison.
+ *
+ * <p>
+ * Theoretically no-one should ever see this exception but mistakes do happen.
+ *
+ * <p>
+ * If it occurs check that you're not accidentally comparing LargeObject columns
+ * (JavaObject, CLOB, BLOB, etc) as they support very few comparisons.
+ *
+ * <p>
+ * Alternatively inform the developers and they will fix it.
+ *
+ * @author Gregory Graham
+ */
+public class ComparisonBetweenTwoDissimilarTypes extends DBRuntimeException {
+
+	public ComparisonBetweenTwoDissimilarTypes(DBDatabase db, DBExpression genericExpression, DBExpression firstValue) {
+		super("Attempt To Compared Two Dissimilar Types: "
+				+ genericExpression.toSQLString(db)
+				+ " is a " + genericExpression.getClass().getSimpleName()
+				+ " and cannot be compared to " + firstValue.toSQLString(db)
+				+ " which is a " + firstValue.getClass().getSimpleName());
+	}
+
+}
