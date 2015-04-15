a69b8ef5c2be5cd9ffadd5b0e67fd7750ebe5eb1
toaomalkster <toaomalkster@bdf2cdf3-4205-4e92-8bbf-280280a2a4f9>
2015-04-15 00:39:22 +0000

    Added new tests using different sources of values.

--- trunk/src/test/java/nz/co/gregs/dbvolution/datatypes/DBIntegerTest.java
+++ trunk/src/test/java/nz/co/gregs/dbvolution/datatypes/DBIntegerTest.java
@@ -3,10 +3,25 @@ package nz.co.gregs.dbvolution.datatypes;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import nz.co.gregs.dbvolution.DBQuery;
+import nz.co.gregs.dbvolution.DBQueryRow;
+import nz.co.gregs.dbvolution.example.CarCompany;
+import nz.co.gregs.dbvolution.example.Marque;
+import nz.co.gregs.dbvolution.generic.AbstractTest;
+
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
-public class DBIntegerTest {
+public class DBIntegerTest extends AbstractTest {
+	
+	public DBIntegerTest(Object testIterationName, Object db) {
+		super(testIterationName, db);
+	}
+
 	@Ignore("DBOperator and QueryableDatatype overload instead of override Object.equals()")
 	@Test
 	public void equalGivenSameLiteralValue() {
@@ -23,6 +38,33 @@ public class DBIntegerTest {
 		DBInteger int2 = new DBInteger();
 		int1.setValue(100);
 		int2.setValue(101);
+		assertThat(int1.equals(int2), is(false));
+	}
+
+	@Ignore("DBOperator and QueryableDatatype overload instead of override Object.equals()")
+	@Test
+	public void equalGivenSameValuesFromDifferentSources() throws SQLException {
+		DBInteger int1 = new DBInteger();
+		int1.setValue(1);
+		
+		Marque marqueEx = new Marque();
+		marqueEx.uidMarque.permittedValues(1);
+		List<Marque> results = database.get(marqueEx);
+		DBInteger int2 = results.get(0).uidMarque;
+		
+		assertThat(int1.equals(int2), is(true));
+	}
+
+	@Test
+	public void notEqualGivenDifferentValuesFromDifferentSources() throws SQLException {
+		DBInteger int1 = new DBInteger();
+		int1.setValue(2);
+		
+		Marque marqueEx = new Marque();
+		marqueEx.uidMarque.permittedValues(1);
+		List<Marque> results = database.get(marqueEx);
+		DBInteger int2 = results.get(0).uidMarque;
+		
 		assertThat(int1.equals(int2), is(false));
 	}
 }
