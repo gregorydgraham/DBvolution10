ba0b972ad130104cc8c305d8e7a874da7c727242
gregorydgraham <gregorydgraham@bdf2cdf3-4205-4e92-8bbf-280280a2a4f9>
2015-05-25 01:05:10 +0000

    Fixed H2 intersects test

--- trunk/src/main/java/nz/co/gregs/dbvolution/internal/h2/Line2DFunctions.java
+++ trunk/src/main/java/nz/co/gregs/dbvolution/internal/h2/Line2DFunctions.java
@@ -17,12 +17,14 @@ package nz.co.gregs.dbvolution.internal.h2;
 
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author gregorygraham
  */
-public enum Line2DFunctions implements Functions{
+public enum Line2DFunctions implements Functions {
 
 	CREATE("DBV_CREATE_LINE2D_FROM_COORDS", "String", "Double... coords", "\n"
 			+ "			Integer numberOfArguments = coords.length;\n"
@@ -135,8 +137,50 @@ public enum Line2DFunctions implements Functions{
 			+ "				return resultString;\n"
 			+ "			}"),
 	DIMENSION("DBV_LINE2D_DIMENSION", "Integer", "String firstLine", "return 1;"),
-	ASTEXT("DBV_LINE2D_ASTEXT", "String", "String firstLine", "return firstLine;"), 
-	INTERSECTS_LINE2D("DBV_LINE2D_INTERSECTS_LINE2D", "Boolean", "String firstLine, String secondLine", "return null;");
+	ASTEXT("DBV_LINE2D_ASTEXT", "String", "String firstLine", "return firstLine;"),
+	INTERSECTS_LINE2D("DBV_LINE2D_INTERSECTS_LINE2D", "Boolean", "String firstLine, String secondLine", "\n"
+			+ "		if (firstLine == null || secondLine == null) {\n"
+			+ "			return null;\n"
+			+ "		}\n"
+			+ "		String[] split1 = firstLine.split(\"[ (),]+\");\n"
+			+ "		String[] split2 = secondLine.split(\"[ (),]+\");\n"
+			+ "		for (int index1 = 0; index1 < split1.length - 3; index1 += 2) {\n"
+			+ "			double p0x = Double.parseDouble(split1[index1 + 1]);\n"
+			+ "			double p0y = Double.parseDouble(split1[index1 + 2]);\n"
+			+ "			double p1x = Double.parseDouble(split1[index1 + 3]);\n"
+			+ "			double p1y = Double.parseDouble(split1[index1 + 4]);\n"
+			+ "\n"
+			+ "			for (int index2 = 0; index2 < split2.length - 3; index2 += 2) {\n"
+			+ "				double p2x = Double.parseDouble(split2[index2 + 1]);\n"
+			+ "				double p2y = Double.parseDouble(split2[index2 + 2]);\n"
+			+ "				double p3x = Double.parseDouble(split2[index2 + 3]);\n"
+			+ "				double p3y = Double.parseDouble(split2[index2 + 4]);\n"
+			+ "\n"
+			+ "				double s1_x, s1_y, s2_x, s2_y;\n"
+			+ "				double i_x, i_y;\n"
+			+ "				s1_x = p1x - p0x;\n"
+			+ "				s1_y = p1y - p0y;\n"
+			+ "				s2_x = p3x - p2x;\n"
+			+ "				s2_y = p3y - p2y;\n"
+			+ "\n"
+			+ "				double s, t;\n"
+			+ "\n"
+			+ "				s = (-s1_y * (p0x - p2x) + s1_x * (p0y - p2y)) / (-s2_x * s1_y + s1_x * s2_y);\n"
+			+ "				t = (s2_x * (p0y - p2y) - s2_y * (p0x - p2x)) / (-s2_x * s1_y + s1_x * s2_y);\n"
+			+ "\n"
+			+ "				if (s >= 0 && s <= 1 && t >= 0 && t <= 1) {\n"
+			+ "					// Collision detected\n"
+			+ "					return true;\n"
+			+ "					//i_x = p0x + (t * s1_x);\n"
+			+ "					//i_y = p0y + (t * s1_y);\n"
+			+ "					//pointsFound.add(\"POINT (\" + i_x + \" \" + i_y + \")\");\n"
+			+ "				} else {\n"
+			+ "					// No collision\n"
+			+ "					//return null;\n"
+			+ "				}\n"
+			+ "			}\n"
+			+ "		}\n"
+			+ "		return false;");
 
 	private final String functionName;
 	private final String returnType;
@@ -157,12 +201,58 @@ public enum Line2DFunctions implements Functions{
 
 	@Override
 	public void add(Statement stmt) throws SQLException {
-//		try {
-//			stmt.execute("DROP ALIAS " + functionName + ";");
-//		} catch (SQLException sqlex) {
-//			;
-//		}
+		try {
+			stmt.execute("DROP ALIAS " + functionName + ";");
+		} catch (SQLException sqlex) {
+			;
+		}
 		stmt.execute("CREATE ALIAS IF NOT EXISTS " + functionName + " DETERMINISTIC AS $$ \n" + "@CODE " + returnType + " " + functionName + "(" + parameters + ") {\n" + code + "} $$;");
+	}
+
+	private Boolean intersects(String firstLine, String secondLine) {
+		//List<String> pointsFound = new ArrayList<String>();
+		if (firstLine == null || secondLine == null) {
+			return null;
+		}
+		String[] split1 = firstLine.split("[ (),]+");
+		String[] split2 = secondLine.split("[ (),]+");
+		for (int index1 = 0; index1 < split1.length - 3; index1 += 2) {
+			double p0x = Double.parseDouble(split1[index1 + 1]);
+			double p0y = Double.parseDouble(split1[index1 + 2]);
+			double p1x = Double.parseDouble(split1[index1 + 3]);
+			double p1y = Double.parseDouble(split1[index1 + 4]);
+
+			for (int index2 = 0; index2 < split2.length - 3; index2 += 2) {
+				double p2x = Double.parseDouble(split2[index2 + 1]);
+				double p2y = Double.parseDouble(split2[index2 + 2]);
+				double p3x = Double.parseDouble(split2[index2 + 3]);
+				double p3y = Double.parseDouble(split2[index2 + 4]);
+
+				double s1_x, s1_y, s2_x, s2_y;
+				double i_x, i_y;
+				s1_x = p1x - p0x;
+				s1_y = p1y - p0y;
+				s2_x = p3x - p2x;
+				s2_y = p3y - p2y;
+
+				double s, t;
+
+				s = (-s1_y * (p0x - p2x) + s1_x * (p0y - p2y)) / (-s2_x * s1_y + s1_x * s2_y);
+				t = (s2_x * (p0y - p2y) - s2_y * (p0x - p2x)) / (-s2_x * s1_y + s1_x * s2_y);
+
+				if (s >= 0 && s <= 1 && t >= 0 && t <= 1) {
+					// Collision detected
+					return true;
+					//i_x = p0x + (t * s1_x);
+					//i_y = p0y + (t * s1_y);
+					//pointsFound.add("POINT (" + i_x + " " + i_y + ")");
+				} else {
+					// No collision
+					//return null;
+				}
+			}
+		}
+		return false;
 	}
 
 }
