0a02afac3630f42e4a263edefa675b0e24c22e2e
greg <greg@192.168.0.8>
2016-04-24 15:52:43 +1200

    Increased the number of DBRow classes that will be found as the DateRepeat test changes have introduced 3 more

--- src/test/java/nz/co/gregs/dbvolution/reflection/DataModelTest.java
+++ src/test/java/nz/co/gregs/dbvolution/reflection/DataModelTest.java
@@ -101,7 +101,7 @@ public class DataModelTest extends AbstractTest {
 		for (Class<? extends DBRow> class1 : result) {
 			System.out.println(class1.getName());
 		}
-		Assert.assertThat(result.size(), is(198));
+		Assert.assertThat(result.size(), is(201));
 	}
 
 	@Test
