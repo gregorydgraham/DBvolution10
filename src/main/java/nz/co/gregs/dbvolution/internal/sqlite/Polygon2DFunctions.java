0eaa7e25070573ef345b7dd8361a9b640979d7eb
Gregory Graham <gregory@gregs.co.nz>
2017-04-07 17:37:31 +1200

    Removed unnecessary import

--- src/main/java/nz/co/gregs/dbvolution/internal/sqlite/Polygon2DFunctions.java
+++ src/main/java/nz/co/gregs/dbvolution/internal/sqlite/Polygon2DFunctions.java
@@ -29,7 +29,6 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import nz.co.gregs.dbvolution.databases.SQLiteDB;
-import nz.co.gregs.dbvolution.databases.definitions.SQLiteDefinition;
 import org.sqlite.Function;
 
 /**
