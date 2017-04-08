5bc98fba264f79018435e7689127aca04e199ccc
Gregory Graham <gregory@gregs.co.nz>
2017-04-08 21:58:42 +1200

    Removed unnecessary prints and improved 3 tests

--- src/test/java/nz/co/gregs/dbvolution/PrimaryKeylessTableTest.java
+++ src/test/java/nz/co/gregs/dbvolution/PrimaryKeylessTableTest.java
@@ -16,12 +16,15 @@
 package nz.co.gregs.dbvolution;
 
 import java.sql.SQLException;
+import java.util.List;
 import nz.co.gregs.dbvolution.example.CarCompany;
 import nz.co.gregs.dbvolution.example.CompanyLogo;
 import nz.co.gregs.dbvolution.example.LinkCarCompanyAndLogo;
 import nz.co.gregs.dbvolution.example.Marque;
 import nz.co.gregs.dbvolution.exceptions.AccidentalCartesianJoinException;
 import nz.co.gregs.dbvolution.generic.AbstractTest;
+import static org.hamcrest.Matchers.*;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -40,10 +43,17 @@ public class PrimaryKeylessTableTest extends AbstractTest {
 		LinkCarCompanyAndLogo link = new LinkCarCompanyAndLogo();
 		DBQuery dbQuery = database.getDBQuery(carCompany, link);
 		dbQuery.setBlankQueryAllowed(true);
-		dbQuery.print();
-		dbQuery = database.getDBQuery(new CompanyLogo(), link);
-		dbQuery.setBlankQueryAllowed(true);
-		dbQuery.print();
+		String sqlForQuery = dbQuery.getSQLForQuery();
+		Assert.assertThat(testableSQL(sqlForQuery), is(testableSQL("SELECT __78874071.name DB_241667647,\n"
+				+ "__78874071.uid_carcompany DB112832814,\n"
+				+ "_1617907935.fk_car_company DB_238514883,\n"
+				+ "_1617907935.fk_company_logo DB_1915875486\n"
+				+ " FROM  car_company AS __78874071 \n"
+				+ " INNER JOIN lt_carco_logo AS _1617907935  ON( _1617907935.fk_car_company = __78874071.uid_carcompany ) \n"
+				+ "\n"
+				+ ";")));
+		List<DBQueryRow> allRows = dbQuery.getAllRows();
+		Assert.assertThat(allRows.size(), is(0));
 	}
 
 	@Test
@@ -53,17 +63,16 @@ public class PrimaryKeylessTableTest extends AbstractTest {
 
 		DBQuery dbQuery = database.getDBQuery(carCompany, link, new CompanyLogo());
 		dbQuery.setBlankQueryAllowed(true);
-		dbQuery.print();
+		String sqlForQuery = dbQuery.getSQLForQuery();
+		Assert.assertThat(testableSQL(sqlForQuery), is(testableSQL("select __78874071.name db_241667647, __78874071.uid_carcompany db112832814, _1617907935.fk_car_company db_238514883, _1617907935.fk_company_logo db_1915875486, _1159239592.logo_id db_1579317226, _1159239592.car_company_fk db1430605643, _1159239592.image_file db1622411417, _1159239592.image_name db1622642088 from car_company as __78874071 inner join lt_carco_logo as _1617907935 on( _1617907935.fk_car_company = __78874071.uid_carcompany ) inner join companylogo as _1159239592 on( _1159239592.car_company_fk = __78874071.uid_carcompany and _1617907935.fk_company_logo = _1159239592.logo_id ) ;")));
+		List<DBQueryRow> allRows = dbQuery.getAllRows();
+		Assert.assertThat(allRows.size(), is(0));
 	}
 
-		@Test
+	@Test(expected = AccidentalCartesianJoinException.class)
 	public void testCartesianJoinProtection() throws SQLException, Exception {
-		try {
-			DBQuery dbQuery = database.getDBQuery(new Marque(), new CompanyLogo());
-			dbQuery.setBlankQueryAllowed(true);
-			dbQuery.print();
-			throw new Exception("Should have thrown an AccidentalCartesianJoinException here.");
-		} catch (AccidentalCartesianJoinException ex) {
-		}
+		DBQuery dbQuery = database.getDBQuery(new Marque(), new CompanyLogo());
+		dbQuery.setBlankQueryAllowed(true);
+		dbQuery.print();
 	}
 }
