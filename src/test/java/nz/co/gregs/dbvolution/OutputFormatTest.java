0743a405a9f5ccacc6bca714ce0ec5847f07b8f3
Gregory Graham <gregory@gregs.co.nz>
2017-04-08 21:33:54 +1200

    Removed unnecessary prints & improved a test

--- src/test/java/nz/co/gregs/dbvolution/OutputFormatTest.java
+++ src/test/java/nz/co/gregs/dbvolution/OutputFormatTest.java
@@ -26,7 +26,6 @@ public class OutputFormatTest extends AbstractTest {
 
 	@Test
 	public void testFormatAllTSV() throws SQLException {
-		System.out.println("nz.co.gregs.dbvolution.OutputFormatTest.testFormatAllTSV()");
 
 		DBQuery dbQuery = database.getDBQuery();
 		CarCompany carCompany = new CarCompany();
@@ -40,7 +39,7 @@ public class OutputFormatTest extends AbstractTest {
 		Assert.assertThat(allRows.size(), is(2));
 
 		String formatDBQueryRows = OutputFormat.TSV.formatDBQueryRows("headerrow", "headercell", "rowstyle", DATETIME_FORMAT, allRows);
-		System.out.println(formatDBQueryRows);
+
 		final String newline = System.getProperty("line.separator");
 		Assert.assertThat(formatDBQueryRows.replaceAll(".00000", ""), is("Marque:numericCode	Marque:uidMarque	Marque:isUsedForTAFROs	Marque:statusClassID	Marque:individualAllocationsAllowed	Marque:updateCount	Marque:auto_created	Marque:name	Marque:pricingCodePrefix	Marque:reservationsAllowed	Marque:creationDate	Marque:enabled	Marque:carCompany	CarCompany:name	CarCompany:uidCarCompany" + newline
 				+ "	1	False	1246974		0		TOYOTA		Y	23/Mar/2013 12:34:56	true	1	TOYOTA	1" + newline
@@ -49,8 +48,7 @@ public class OutputFormatTest extends AbstractTest {
 
 	@Test
 	public void testFormatAllTableTSV() throws SQLException {
-		System.out.println("nz.co.gregs.dbvolution.OutputFormatTest.testFormatAllTableTSV()");
-
+		final String newline = System.getProperty("line.separator");
 		CarCompany carCompany = new CarCompany();
 		carCompany.name.permittedValues("TOYOTA");
 		DBTable<CarCompany> dbTable = database.getDBTable(carCompany);
@@ -61,16 +59,15 @@ public class OutputFormatTest extends AbstractTest {
 
 		String formatDBRows = OutputFormat.TSV.formatDBRows("headerrow", "headercell", "rowstyle", DATETIME_FORMAT, allRowsArray);
 		System.out.println(formatDBRows);
+		Assert.assertThat(formatDBRows, is("name	uidCarCompany" + newline +  "TOYOTA	1" + newline));
+
 		formatDBRows = OutputFormat.TSV.formatDBRows(DATETIME_FORMAT, allRowsArray);
-		System.out.println(formatDBRows);
-		final String newline = System.getProperty("line.separator");
+
 		Assert.assertThat(formatDBRows, is("name	uidCarCompany" + newline + "TOYOTA	1" + newline));
 	}
 
 	@Test
 	public void testFormatAllTableCSV() throws SQLException {
-		System.out.println("nz.co.gregs.dbvolution.OutputFormatTest.testFormatAllTableTSV()");
-
 		CarCompany carCompany = new CarCompany();
 		carCompany.name.permittedValues("TOYOTA");
 		DBTable<CarCompany> dbTable = database.getDBTable(carCompany);
@@ -82,17 +79,16 @@ public class OutputFormatTest extends AbstractTest {
 		final String newline = System.getProperty("line.separator");
 
 		String formatDBRows = OutputFormat.CSV.formatDBRows("headerrow", "headercell", "rowstyle", DATETIME_FORMAT, allRowsArray);
-		System.out.println(formatDBRows);
+
 		Assert.assertThat(formatDBRows, is("\"name\", \"uidCarCompany\"" + newline + "\"TOYOTA\", \"1\"" + newline));
 
 		formatDBRows = OutputFormat.CSV.formatDBRows(DATETIME_FORMAT, allRowsArray);
-		System.out.println(formatDBRows);
+
 		Assert.assertThat(formatDBRows, is("\"name\", \"uidCarCompany\"" + newline + "\"TOYOTA\", \"1\"" + newline));
 	}
 
 	@Test
 	public void testFormatAllCSV() throws SQLException {
-		System.out.println("nz.co.gregs.dbvolution.OutputFormatTest.testFormatAllTSV()");
 
 		DBQuery dbQuery = database.getDBQuery();
 		CarCompany carCompany = new CarCompany();
@@ -107,15 +103,13 @@ public class OutputFormatTest extends AbstractTest {
 		Assert.assertThat(allRows.size(), is(2));
 
 		String formatDBQueryRows = OutputFormat.CSV.formatDBQueryRows("headerrow", "headercell", "rowstyle", DATETIME_FORMAT, allRows);
-		System.out.println(formatDBQueryRows);
+		
 		final String newline = System.getProperty("line.separator");
 		Assert.assertThat(formatDBQueryRows.replaceAll(".00000", ""), is(("\"Marque:numericCode\", \"Marque:uidMarque\", \"Marque:isUsedForTAFROs\", \"Marque:statusClassID\", \"Marque:individualAllocationsAllowed\", \"Marque:updateCount\", \"Marque:auto_created\", \"Marque:name\", \"Marque:pricingCodePrefix\", \"Marque:reservationsAllowed\", \"Marque:creationDate\", \"Marque:enabled\", \"Marque:carCompany\", \"CarCompany:name\", \"CarCompany:uidCarCompany\"" + newline + "\"\", \"1\", \"False\", \"1246974\", \"\", \"0\", \"\", \"TOYOTA\", \"\", \"Y\", \"23/Mar/2013 12:34:56\", \"true\", \"1\", \"TOYOTA\", \"1\"" + newline + "\"\", \"4896300\", \"False\", \"1246974\", \"\", \"2\", \"UV\", \"HYUNDAI\", \"\", \"Y\", \"23/Mar/2013 12:34:56\", \"\", \"1\", \"TOYOTA\", \"1\"" + newline)));
 	}
 
 	@Test
 	public void testFormatAllTableHTML() throws SQLException {
-		System.out.println("nz.co.gregs.dbvolution.OutputFormatTest.testFormatAllTableTSV()");
-
 		CarCompany carCompany = new CarCompany();
 		carCompany.name.permittedValues("TOYOTA");
 		DBTable<CarCompany> dbTable = database.getDBTable(carCompany);
@@ -127,20 +121,18 @@ public class OutputFormatTest extends AbstractTest {
 		final String newline = System.getProperty("line.separator");
 
 		String formatDBRows = OutputFormat.HTMLTABLE.formatDBRows("headerrow", "headercell", "rowstyle", DATETIME_FORMAT, allRowsArray);
-		System.out.println(formatDBRows);
+		
 		Assert.assertThat(formatDBRows.replaceAll(".00000", ""), is("<tr class=\"headerrow\"><th class=\"headercell\">name</th><th class=\"headercell\">uidCarCompany</th></tr>" + newline
 				+ "<tr class=\"rowstyle\"><td class=\"rowstyle\">TOYOTA</td><td class=\"rowstyle\">1</td></tr>" + newline));
 
 		formatDBRows = OutputFormat.HTMLTABLE.formatDBRows(DATETIME_FORMAT, allRowsArray);
-		System.out.println(formatDBRows);
+		
 		Assert.assertThat(formatDBRows.replaceAll(".00000", ""), is("<tr class=\"\"><th class=\"\">name</th><th class=\"\">uidCarCompany</th></tr>" + newline
 				+ "<tr class=\"\"><td class=\"\">TOYOTA</td><td class=\"\">1</td></tr>" + newline));
 	}
 
 	@Test
 	public void testFormatAllHTML() throws SQLException {
-		System.out.println("nz.co.gregs.dbvolution.OutputFormatTest.testFormatAllTSV()");
-
 		DBQuery dbQuery = database.getDBQuery();
 		CarCompany carCompany = new CarCompany();
 		carCompany.name.permittedValues("TOYOTA");
@@ -154,7 +146,7 @@ public class OutputFormatTest extends AbstractTest {
 		Assert.assertThat(allRows.size(), is(2));
 
 		String formatDBQueryRows = OutputFormat.HTMLTABLE.formatDBQueryRows("headerrow", "headercell", "rowstyle", DATETIME_FORMAT, allRows);
-		System.out.println(formatDBQueryRows);
+		
 		final String newline = System.getProperty("line.separator");
 		Assert.assertThat(formatDBQueryRows.replaceAll(".00000", ""), is("<tr class=\"headerrow\"><th class=\"headercell\">Marque:numericCode</th><th class=\"headercell\">Marque:uidMarque</th><th class=\"headercell\">Marque:isUsedForTAFROs</th><th class=\"headercell\">Marque:statusClassID</th><th class=\"headercell\">Marque:individualAllocationsAllowed</th><th class=\"headercell\">Marque:updateCount</th><th class=\"headercell\">Marque:auto_created</th><th class=\"headercell\">Marque:name</th><th class=\"headercell\">Marque:pricingCodePrefix</th><th class=\"headercell\">Marque:reservationsAllowed</th><th class=\"headercell\">Marque:creationDate</th><th class=\"headercell\">Marque:enabled</th><th class=\"headercell\">Marque:carCompany</th><th class=\"headercell\">CarCompany:name</th><th class=\"headercell\">CarCompany:uidCarCompany</th></tr>" + newline
 				+ "<tr class=\"rowstyle\"><td class=\"rowstyle\"></td><td class=\"rowstyle\">1</td><td class=\"rowstyle\">False</td><td class=\"rowstyle\">1246974</td><td class=\"rowstyle\"></td><td class=\"rowstyle\">0</td><td class=\"rowstyle\"></td><td class=\"rowstyle\">TOYOTA</td><td class=\"rowstyle\"></td><td class=\"rowstyle\">Y</td><td class=\"rowstyle\">23/Mar/2013 12:34:56</td><td class=\"rowstyle\">true</td><td class=\"rowstyle\">1</td><td class=\"rowstyle\">TOYOTA</td><td class=\"rowstyle\">1</td></tr>" + newline
