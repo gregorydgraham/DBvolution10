948bf2de9f77932e5f30adbff6f4d64b920f2c79
Gregory Graham <gregory@gregs.co.nz>
2018-09-05 12:37:55 +1200

    Improved JavaDoc

--- src/main/java/nz/co/gregs/dbvolution/databases/DatabaseConnectionSettings.java
+++ src/main/java/nz/co/gregs/dbvolution/databases/DatabaseConnectionSettings.java
@@ -120,6 +120,78 @@ public class DatabaseConnectionSettings {
 		super();
 	}
 
+	/**
+	 * A standardized collection of the database connection settings.
+	 *
+	 * <p>
+	 * This object is a bean to provide a consistent way of defining a the
+	 * connection details needed to connect to a database.</p>
+	 *
+	 * <p>
+	 * Connection details can be grouped as username/password, URL, settings, and
+	 * extras.</p>
+	 *
+	 * <p>
+	 * Username and password are generally required to connect to a database and
+	 * are provided to the connection separately from the url, settings, and
+	 * extras.</p>
+	 *
+	 * <p>
+	 * URL, settings, and extras are used to create the JDBC connection URL to the
+	 * database and, with the username/password, to connection to the
+	 * database.</p>
+	 *
+	 * <p>
+	 * If the URL is supplied it will be used as provided and settings and extras
+	 * will be ignored. This is reflected in the 2 standard constructors for
+	 * DatabaseConnectionSettings:
+	 * {@link #DatabaseConnectionSettings(java.lang.String, java.lang.String, java.lang.String) one for username/password/url}
+	 * and
+	 * {@link #DatabaseConnectionSettings(java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.util.Map) one for username/password/settings/extras}.</p>
+	 *
+	 * <p>
+	 * Without an explicit URL the settings (host, port, instance, database,
+	 * schema) and extras will be combined to create the JDBC URL. This
+	 * combination is deferred to the appropriate DBDatabase class and its version
+	 * of null {@link DBDatabase#getUrlFromSettings(nz.co.gregs.dbvolution.databases.DatabaseConnectionSettings)
+	 * }</p>
+	 *
+	 * <p>
+	 * Extras are any miscellaneous and database specific settings that are added
+	 * to the end of the JDBC URL to tweak the connection or database. Generally
+	 * these are very database specific and will not work as expected for any
+	 * other providers product.</p>
+	 *
+	 * <p>
+	 * There is some confusion in the Database/JDBC world as to what some of the
+	 * settings names (host, port, instance, database, schema) mean. For the
+	 * purposes of DatabaseConnectionSettings:</p>
+	 * <ul>
+	 * <li>Host is the server name or Internet address of the database server, for
+	 * instance db1.acme.com or 101.203.54.9.</li>
+	 * <li>Port is the port number on the host that the database will accept
+	 * connections from, for instance 1336</li>
+	 * <li>Instance is the particular application or service that is providing the
+	 * database if the database application is capable of running multiple
+	 * instances on one server. Many databases are not and this setting should be
+	 * ignored for those that cannot.</li>
+	 * <li>Database is the named database within the application that the
+	 * connection should use. Database is the central concept that all database
+	 * providers implement. File based databases should use this to provide the
+	 * file name.</li>
+	 * <li>Schema is the level below database. It is optional or irrelevant for
+	 * many RDBMSs or user setups. This is primarily where a user can create their
+	 * own groupings below the database that they have been assigned to. Schema
+	 * can also be specified using {@link DBTableName} when the schema name is
+	 * unchanging.</li>
+	 * </ul>
+	 *
+	 *
+	 * @author Gregory Graham
+	 * @param url
+	 * @param username
+	 * @param password
+	 */
 	public DatabaseConnectionSettings(String url, String username, String password) {
 		super();
 		this.url = url;
@@ -127,6 +199,83 @@ public class DatabaseConnectionSettings {
 		this.password = password;
 	}
 
+	/**
+	 * A standardized collection of the database connection settings.
+	 *
+	 * <p>
+	 * This object is a bean to provide a consistent way of defining a the
+	 * connection details needed to connect to a database.</p>
+	 *
+	 * <p>
+	 * Connection details can be grouped as username/password, URL, settings, and
+	 * extras.</p>
+	 *
+	 * <p>
+	 * Username and password are generally required to connect to a database and
+	 * are provided to the connection separately from the url, settings, and
+	 * extras.</p>
+	 *
+	 * <p>
+	 * URL, settings, and extras are used to create the JDBC connection URL to the
+	 * database and, with the username/password, to connection to the
+	 * database.</p>
+	 *
+	 * <p>
+	 * If the URL is supplied it will be used as provided and settings and extras
+	 * will be ignored. This is reflected in the 2 standard constructors for
+	 * DatabaseConnectionSettings:
+	 * {@link #DatabaseConnectionSettings(java.lang.String, java.lang.String, java.lang.String) one for username/password/url}
+	 * and
+	 * {@link #DatabaseConnectionSettings(java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.util.Map) one for username/password/settings/extras}.</p>
+	 *
+	 * <p>
+	 * Without an explicit URL the settings (host, port, instance, database,
+	 * schema) and extras will be combined to create the JDBC URL. This
+	 * combination is deferred to the appropriate DBDatabase class and its version
+	 * of null {@link DBDatabase#getUrlFromSettings(nz.co.gregs.dbvolution.databases.DatabaseConnectionSettings)
+	 * }</p>
+	 *
+	 * <p>
+	 * Extras are any miscellaneous and database specific settings that are added
+	 * to the end of the JDBC URL to tweak the connection or database. Generally
+	 * these are very database specific and will not work as expected for any
+	 * other providers product.</p>
+	 *
+	 * <p>
+	 * There is some confusion in the Database/JDBC world as to what some of the
+	 * settings names (host, port, instance, database, schema) mean. For the
+	 * purposes of DatabaseConnectionSettings:</p>
+	 * <ul>
+	 * <li>Host is the server name or Internet address of the database server, for
+	 * instance db1.acme.com or 101.203.54.9.</li>
+	 * <li>Port is the port number on the host that the database will accept
+	 * connections from, for instance 1336</li>
+	 * <li>Instance is the particular application or service that is providing the
+	 * database if the database application is capable of running multiple
+	 * instances on one server. Many databases are not and this setting should be
+	 * ignored for those that cannot.</li>
+	 * <li>Database is the named database within the application that the
+	 * connection should use. Database is the central concept that all database
+	 * providers implement. File based databases should use this to provide the
+	 * file name.</li>
+	 * <li>Schema is the level below database. It is optional or irrelevant for
+	 * many RDBMSs or user setups. This is primarily where a user can create their
+	 * own groupings below the database that they have been assigned to. Schema
+	 * can also be specified using {@link DBTableName} when the schema name is
+	 * unchanging.</li>
+	 * </ul>
+	 *
+	 *
+	 * @author Gregory Graham
+	 * @param host
+	 * @param port
+	 * @param instance
+	 * @param database
+	 * @param schema
+	 * @param username
+	 * @param password
+	 * @param extras
+	 */
 	public DatabaseConnectionSettings(String host, String port, String instance, String database, String schema, String username, String password, Map<String, String> extras) {
 		super();
 		this.host = host;
@@ -155,9 +304,6 @@ public class DatabaseConnectionSettings {
 
 	public DBDatabase createDBDatabase() throws ClassNotFoundException, NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		Class<?> dbDatabaseClass = Class.forName(this.getDBDatabase());
-//		String jdbcUrl = this.getUrl();
-//		String user = this.getUsername();
-//		String pass = this.getPassword();
 		Constructor<?> constructor = dbDatabaseClass.getConstructor(DatabaseConnectionSettings.class);
 		if (constructor == null) {
 			return null;
