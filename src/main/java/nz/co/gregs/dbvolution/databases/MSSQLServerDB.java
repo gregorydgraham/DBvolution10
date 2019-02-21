19c60f225a7d692186f99e1e81264b23b9d401a0
Gregory Graham <gregory@gregs.co.nz>
2019-02-22 11:57:29 +1300

    Improved formatting

--- src/main/java/nz/co/gregs/dbvolution/databases/MSSQLServerDB.java
+++ src/main/java/nz/co/gregs/dbvolution/databases/MSSQLServerDB.java
@@ -214,13 +214,13 @@ public class MSSQLServerDB extends DBDatabase implements SupportsPolygonDatatype
 			fn.add(statement);
 		}
 	}
+
 	//Invalid object name 'TableThatDoesntExistOnTheCluster'.
 	private final static Pattern NONEXISTANT_TABLE_PATTERN = Pattern.compile("Invalid object name '[^\"]*\'.");
 	//There is already an object named 'TableThatDoesExistOnTheCluster' in the database.
 	private final static Pattern CREATING_EXISTING_TABLE_PATTERN = Pattern.compile("There is already an object named '[^\"]*\' in the database.");
 	//Cannot find the object "TableThatDoesntExistOnTheCluster" because it does not exist or you do not have permissions.
 	private final static Pattern UNABLE_TO_FIND_DATABASE_OBJECT_PATTERN = Pattern.compile("Cannot find the object \"[^\"]*\" because it does not exist or you do not have permissions.");
-	
 
 	@Override
 	public ResponseToException addFeatureToFixException(Exception exp) throws Exception {
