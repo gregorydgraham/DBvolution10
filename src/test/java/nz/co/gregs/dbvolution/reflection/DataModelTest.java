78653176a1afbb1e02c6c57ab62a77f674b4173f
Gregory Graham <gregory@gregs.co.nz>
2019-09-02 14:35:43 +1200

    Updated class numbers for the new classes in LocalDateExpressionTest

--- src/test/java/nz/co/gregs/dbvolution/reflection/DataModelTest.java
+++ src/test/java/nz/co/gregs/dbvolution/reflection/DataModelTest.java
@@ -139,13 +139,13 @@ public class DataModelTest extends AbstractTest {
 	@Test
 	public void testGetDBRowClasses() {
 		Set<Class<? extends DBRow>> result = DataModel.getDBRowSubclasses();
-		Assert.assertThat(result.size(), is(287));
+		Assert.assertThat(result.size(), is(289));
 	}
 
 	@Test
 	public void testGetDBRowDirectSubclasses() {
 		Set<Class<? extends DBRow>> result = DataModel.getDBRowDirectSubclasses();
-		Assert.assertThat(result.size(), is(114));
+		Assert.assertThat(result.size(), is(115));
 	}
 
 	@Test
