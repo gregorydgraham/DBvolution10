b57e9efb8f5d8051e62a8f8b460bd5f2d2077605
Gregory Graham <gregory@gregs.co.nz>
2019-09-09 15:50:39 +1200

    Added window function testing for booleans

--- src/test/java/nz/co/gregs/dbvolution/expressions/BooleanExpressionTest.java
+++ src/test/java/nz/co/gregs/dbvolution/expressions/BooleanExpressionTest.java
@@ -100,7 +100,7 @@ public class BooleanExpressionTest extends AbstractTest {
 		DBQuery dbQuery = database.getDBQuery(marque);
 
 		dbQuery.addCondition(BooleanExpression.nullBoolean().isNull());
-		
+
 		List<DBQueryRow> allRows = dbQuery.getAllRows();
 
 		Assert.assertThat(allRows.size(), is(22));
@@ -359,7 +359,7 @@ public class BooleanExpressionTest extends AbstractTest {
 
 		dbQuery = database.getDBQuery(carCo);
 		dbQuery.addCondition(carCo.column(carCo.name).isIn(strs));
-		
+
 		allRows = dbQuery.getAllRows();
 
 		Assert.assertThat(allRows.size(), is(3));
@@ -1020,4 +1020,93 @@ public class BooleanExpressionTest extends AbstractTest {
 		allRows = dbQuery.getAllRows();
 		Assert.assertThat(allRows.size(), is(6));
 	}
+
+	@Test
+	public void testWindowingFunctions() throws SQLException {
+		MarqueWithWindowingFunctions marq = new MarqueWithWindowingFunctions();
+
+		DBQuery query = database.getDBQuery(marq)
+				.setBlankQueryAllowed(true)
+				.setSortOrder(
+						marq.column(marq.carCompany).ascending(),
+						marq.column(marq.uidMarque).ascending()
+				);
+
+		List<DBQueryRow> allRows = query.getAllRows();
+		query.printSQLForQuery();
+		Assert.assertThat(allRows.size(), is(22));
+
+		MarqueWithWindowingFunctions got;// = allRows.get(0).get(marq);
+		ArrayList<Object[]> expectedValues = new ArrayList<>();
+		
+		expectedValues.add(new Object[]{3, 1, 1, 1, null, null});
+		expectedValues.add(new Object[]{3, 2, 1, 1, true, false});
+		expectedValues.add(new Object[]{3, 3, 1, 1, (null), (null)});
+		expectedValues.add(new Object[]{3, 4, 0, 0, false, (null)});
+		expectedValues.add(new Object[]{3, 5, 0, 0, (null), (null)});
+		expectedValues.add(new Object[]{3, 6, 0, 0, (null), true});
+		expectedValues.add(new Object[]{3, 7, 1, 1, (null), (null)});
+		expectedValues.add(new Object[]{3, 8, 1, 1, true, (null)});
+		expectedValues.add(new Object[]{3, 9, 1, 1, (null), (null)});
+		expectedValues.add(new Object[]{3, 10, 1, 1, (null), (null)});
+		expectedValues.add(new Object[]{3, 11, 1, 1, (null), (null)});
+		expectedValues.add(new Object[]{3, 12, 1, 1, (null), (null)});
+		expectedValues.add(new Object[]{3, 13, 1, 1, (null), (null)});
+		expectedValues.add(new Object[]{3, 14, 1, 1, (null), (null)});
+		expectedValues.add(new Object[]{3, 15, 1, 1, (null), (null)});
+		expectedValues.add(new Object[]{3, 16, 1, 1, (null), (null)});
+		expectedValues.add(new Object[]{3, 17, 1, 1, (null), (null)});
+		expectedValues.add(new Object[]{3, 18, 1, 1, (null), (null)});
+		expectedValues.add(new Object[]{3, 19, 1, 1, (null), (null)});
+		expectedValues.add(new Object[]{3, 20, 1, 1, (null), (null)});
+		expectedValues.add(new Object[]{3, 21, 1, 1, (null), (null)});
+		expectedValues.add(new Object[]{3, 22, 1, 1, (null), (null)});
+
+		for (int i = 0; i < allRows.size(); i++) {
+			got = allRows.get(i).get(marq);
+			System.out.println("" + got.toString());
+			Object[] expect = expectedValues.get(i);
+			Assert.assertThat(got.countOfAllRows.intValue(), is((Integer) expect[0]));
+			Assert.assertThat(got.rowNumber.intValue(), is((Integer) expect[1]));
+			Assert.assertThat(got.countOfEnabled.intValue(), is((Integer) expect[2]));
+			Assert.assertThat(got.rowWithinCarCo.intValue(), is((Integer) expect[3]));
+			Assert.assertThat(got.lag.getValue(), is((Boolean) expect[4]));
+			Assert.assertThat(got.lead.getValue(), is((Boolean) expect[5]));
+		}
+	}
+
+	public static class MarqueWithWindowingFunctions extends Marque {
+
+		private static final long serialVersionUID = 1L;
+
+		@DBColumn
+		DBNumber countOfAllRows = new DBNumber(this.column(this.enabled).count().over().allRows());
+		@DBColumn
+		DBNumber rowNumber = new DBNumber(this.column(this.uidMarque).count().over().AllRowsAndOrderBy(this.column(this.carCompany).ascending(), this.column(this.uidMarque).ascending()));
+		@DBColumn
+		DBNumber countOfEnabled = new DBNumber(this.column(this.enabled).count().over().partition(this.column(this.carCompany)).unordered());
+		@DBColumn
+		DBNumber rowWithinCarCo = new DBNumber(this.column(this.enabled).count()
+				.over()
+				.partition(this.column(this.carCompany))
+				.orderBy(this.column(this.carCompany).ascending())
+				.defaultFrame());
+		@DBColumn
+		DBBoolean lag
+				= this.column(this.enabled)
+						.previousRowValue()
+						.allRows()
+						.orderBy(this.column(this.carCompany).ascending(), this.column(this.uidMarque).ascending())
+						.asExpressionColumn();
+		@DBColumn
+		DBBoolean lead = new DBBoolean(this.column(
+				this.enabled)
+				.nextRowValue()
+				.AllRowsAndOrderBy(
+						this.column(this.carCompany).ascending(),
+						this.column(this.uidMarque).ascending()
+				)
+		);
+	}
+
 }
