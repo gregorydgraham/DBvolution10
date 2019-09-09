e6f9b04e6bcfd3a355fa968dbc2f66654addefa4
Gregory Graham <gregory@gregs.co.nz>
2019-09-10 11:22:31 +1200

    Updated number of DBRow subclasses to account for lag and lead test classes

--- src/test/java/nz/co/gregs/dbvolution/reflection/DataModelTest.java
+++ src/test/java/nz/co/gregs/dbvolution/reflection/DataModelTest.java
@@ -139,7 +139,7 @@ public class DataModelTest extends AbstractTest {
 	@Test
 	public void testGetDBRowClasses() {
 		Set<Class<? extends DBRow>> result = DataModel.getDBRowSubclasses();
-		Assert.assertThat(result.size(), is(289));
+		Assert.assertThat(result.size(), is(295));
 	}
 
 	@Test
