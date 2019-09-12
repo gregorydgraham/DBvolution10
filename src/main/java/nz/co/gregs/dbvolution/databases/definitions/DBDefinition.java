c7476c5fb7d499d270272ccd831552f32e3e54f2
Gregory Graham <gregory@gregs.co.nz>
2019-09-13 11:47:44 +1200

    Change boolean to integer transform to work better with MSSQLServer

--- src/main/java/nz/co/gregs/dbvolution/databases/definitions/DBDefinition.java
+++ src/main/java/nz/co/gregs/dbvolution/databases/definitions/DBDefinition.java
@@ -2312,8 +2312,7 @@ public abstract class DBDefinition implements Serializable {
 	 * integer expression in the SQL.
 	 */
 	public String doBooleanToIntegerTransform(String booleanExpression) {
-		return "case when " + booleanExpression + " is null then null when " + booleanExpression + " then 1 else 0 end";
-//		return doIfThenElseTransform(booleanExpression, "" + 1, "" + 0);
+		return " case when (" + booleanExpression + ") then 1 when not (" + booleanExpression + ") then 0 else null end ";
 	}
 
 	/**
