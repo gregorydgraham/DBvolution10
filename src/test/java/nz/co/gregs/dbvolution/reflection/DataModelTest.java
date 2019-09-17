7dd88a21eb34567fc8efa7ccd147f93f17df845d
Gregory Graham <gregory@gregs.co.nz>
2019-09-17 14:57:39 +1200

    Added extra class because of the new Lag and Lead testing class in BooleanExpressionTest

--- src/test/java/nz/co/gregs/dbvolution/reflection/DataModelTest.java
+++ src/test/java/nz/co/gregs/dbvolution/reflection/DataModelTest.java
@@ -139,7 +139,7 @@ public class DataModelTest extends AbstractTest {
 	@Test
 	public void testGetDBRowClasses() {
 		Set<Class<? extends DBRow>> result = DataModel.getDBRowSubclasses();
-		Assert.assertThat(result.size(), is(295));
+		Assert.assertThat(result.size(), is(296));
 	}
 
 	@Test
