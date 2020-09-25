1abba1ccb2e73b1d65953d1ede0c79b9ad02818b
Gregory Graham <gregory@gregs.co.nz>
2020-09-26 00:24:30 +1200

    Added environment debugging and commands to turn off apparmor to possibly get the MySQL container working on my Jenkins server

--- src/test/java/nz/co/gregs/dbvolution/databases/MySQLContainerDB.java
+++ src/test/java/nz/co/gregs/dbvolution/databases/MySQLContainerDB.java
@@ -30,10 +30,14 @@
  */
 package nz.co.gregs.dbvolution.databases;
 
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import nz.co.gregs.dbvolution.databases.settingsbuilders.MySQLSettingsBuilder;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.testcontainers.containers.JdbcDatabaseContainerProvider;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.jdbc.ConnectionUrl;
@@ -47,19 +51,29 @@ public class MySQLContainerDB extends MySQLDB {
 	private static final long serialVersionUID = 1l;
 	protected final MySQLContainer<?> storedContainer;
 
+	static final private Log LOG = LogFactory.getLog(MySQLContainerDB.class);
+
 	public static MySQLContainerDB getInstance() {
 		return getLabelledInstance("Unlabelled");
 	}
 
 	public static MySQLContainerDB getLabelledInstance(String label) {
-			final FinalisedMySQLContainer newInstance = new FinalisedMySQLContainerProvider().newInstance("latest");
+		final FinalisedMySQLContainer newInstance = new FinalisedMySQLContainerProvider().newInstance("latest");
 		FinalisedMySQLContainer container = newInstance;
-		
-		ContainerUtils.startContainer(container);
+		container.getEnv().stream().forEach(t -> LOG.info("ENV: " + t));
+		container.getEnvMap().entrySet().stream().forEach(t -> LOG.info("ENV: " + t.getKey() + "=>" + t.getValue()));
 
+		ContainerUtils.startContainer(container);
+		try {
+			container.execInContainer(
+					"sudo ln -s /etc/apparmor.d/usr.sbin.mysqld /etc/apparmor.d/disable/",
+					"sudo apparmor_parser -R /etc/apparmor.d/usr.sbin.mysqld");
+		} catch (UnsupportedOperationException | IOException | InterruptedException ex) {
+			Logger.getLogger(MySQLContainerDB.class.getName()).log(Level.SEVERE, null, ex);
+		}
 		try {
 			// create the actual dbdatabase 
-			MySQLContainerDB dbdatabase 
+			MySQLContainerDB dbdatabase
 					= new MySQLContainerDB(container,
 							ContainerUtils.getContainerSettings(new MySQLSettingsBuilder(), container, label)
 					);
