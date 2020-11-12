403e553025eaa0615be52100bd888297e024ba7c
Gregory Graham <gregory@gregs.co.nz>
2020-11-12 14:30:45 +1300

    Corrected expression in debug statement

--- src/main/java/nz/co/gregs/dbvolution/databases/definitions/DBDefinition.java
+++ src/main/java/nz/co/gregs/dbvolution/databases/definitions/DBDefinition.java
@@ -6978,8 +6978,9 @@ public abstract class DBDefinition implements Serializable {
 	}
 
 	public boolean hasLocalDateTimeOffset() {
-		System.out.println("HAS LOCALDATETIME OFFSET: " + (getLocalDateTimeOffsetHours() != 0 && getLocalDateTimeOffsetMinutes() != 0));
-		return getLocalDateTimeOffsetHours() != 0 || getLocalDateTimeOffsetMinutes() != 0;
+		final boolean hasLocalDateTimeOffset = getLocalDateTimeOffsetHours() != 0 || getLocalDateTimeOffsetMinutes() != 0;
+		System.out.println("HAS LOCALDATETIME OFFSET: " + (hasLocalDateTimeOffset));
+		return hasLocalDateTimeOffset;
 	}
 
 	public int getLocalDateTimeOffsetHours() {
