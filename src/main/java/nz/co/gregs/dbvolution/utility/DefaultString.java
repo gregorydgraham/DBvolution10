8c695895ca869648cb60e34bb81dfca37c92126d
Gregory Graham <gregory@gregs.co.nz>
2021-09-29 15:28:25 +1300

    Cleaned up checking code Added null checking methods to be consistent Added JavaDocs

--- src/main/java/nz/co/gregs/dbvolution/utility/DefaultString.java
+++ src/main/java/nz/co/gregs/dbvolution/utility/DefaultString.java
@@ -37,31 +37,126 @@ package nz.co.gregs.dbvolution.utility;
  */
 public class DefaultString {
 
+	/**
+	 * Returns the supplied value or the default if the supplied value is null or
+	 * empty.
+	 *
+	 *
+	 * @param initialValue
+	 * @param defaultValue
+	 * @return the intialValue or the default if the initialValue is null or empty
+	 */
 	public static String check(String initialValue, String defaultValue) {
 		return initialValue == null || initialValue.isEmpty() ? defaultValue : initialValue;
 	}
 
+	/**
+	 * Returns the first value that is neither null nor empty.
+	 *
+	 * <p>
+	 * Returns the empty string if no value is non-null and non-empty.<p>
+	 *
+	 * @param initialValue
+	 * @param defaultValues
+	 * @return the empty string or the first non-null non-empty value
+	 */
 	public static String check(String initialValue, String... defaultValues) {
+		if (isNotEmptyNorNull(initialValue)) {
+			return initialValue;
+		}
 		for (String value : defaultValues) {
-			if (value != null && !value.isEmpty()) {
-				return initialValue == null || initialValue.isEmpty() ? value : initialValue;
+			if (isNotEmptyNorNull(value)) {
+				return value;
 			}
 		}
-		return check(initialValue, "");
+		return "";
 	}
 
+	/**
+	 * Returns the supplied value or the default if the supplied value is null.
+	 *
+	 *
+	 * @param initialValue
+	 * @param nullValue
+	 * @return the intialValue or the default if the initialValue is null
+	 */
 	public static String checkNotNull(String initialValue, String nullValue) {
 		return initialValue == null ? nullValue : initialValue;
 	}
 
+	/**
+	 * Returns the first value that is non-null.
+	 *
+	 * <p>
+	 * Returns the empty string if no value is non-null.<p>
+	 *
+	 * @param initialValue
+	 * @param defaultValues
+	 * @return the empty string or the first non-null non-empty value
+	 */
+	public static String checkNotNull(String initialValue, String... defaultValues) {
+		if (isNotNull(initialValue)) {
+			return initialValue;
+		}
+		for (String value : defaultValues) {
+			if (isNotNull(value)) {
+				return value;
+			}
+		}
+		return "";
+	}
+
+	/**
+	 * Returns the supplied value or the null value if the supplied value is null
+	 * and the empty value if the supplied value is the empty string.
+	 *
+	 *
+	 * @param initialValue
+	 * @param nullValue
+	 * @param emptyValue
+	 * @return the intialValue if it is non-null and non-empty, nullValue if it is
+	 * null, or emptyValue if it is empty
+	 */
 	public static String checkNotNullOrEmpty(String initialValue, String nullValue, String emptyValue) {
 		return initialValue == null ? nullValue : (initialValue.isEmpty() ? emptyValue : initialValue);
 	}
 
-	public static boolean isNotEmptyOrNull(String value) {
+	/**
+	 * Synonym for intialValue == null
+	 *
+	 * @param initialValue
+	 * @return
+	 */
+	public static boolean isNull(String initialValue) {
+		return initialValue == null;
+	}
+
+	/**
+	 * Synonym for intialValue != null
+	 *
+	 * @param initialValue
+	 * @return
+	 */
+	public static boolean isNotNull(String initialValue) {
+		return initialValue != null;
+	}
+
+	/**
+	 * Synonym for value != null && !value.isEmpty() but a lot more concise.
+	 *
+	 * @param value
+	 * @return
+	 */
+	public static boolean isNotEmptyNorNull(String value) {
 		return value != null && !value.isEmpty();
 	}
 
+	/**
+	 * Synonym for value == null || value.isEmpty() but a lot more concise.
+	 *
+	 * @param value
+	 * @return
+	 */
 	public static boolean isEmptyOrNull(String value) {
 		return value == null || value.isEmpty();
 	}
