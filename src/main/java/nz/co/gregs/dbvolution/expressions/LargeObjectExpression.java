4e91a02561e048ee738e5da31197e1e3016adbb2
Gregory Graham <gregory@gregs.co.nz>
2021-10-23 15:56:00 +1300

    Added param descriptions as suggested by the JavaDoc generator

--- src/main/java/nz/co/gregs/dbvolution/expressions/LargeObjectExpression.java
+++ src/main/java/nz/co/gregs/dbvolution/expressions/LargeObjectExpression.java
@@ -16,10 +16,8 @@
 package nz.co.gregs.dbvolution.expressions;
 
 import nz.co.gregs.dbvolution.results.LargeObjectResult;
-import nz.co.gregs.dbvolution.DBQuery;
 import nz.co.gregs.dbvolution.databases.DBDatabase;
 import nz.co.gregs.dbvolution.databases.definitions.DBDefinition;
-import nz.co.gregs.dbvolution.datatypes.DBLargeBinary;
 import nz.co.gregs.dbvolution.datatypes.QueryableDatatype;
 import nz.co.gregs.dbvolution.results.AnyComparable;
 import nz.co.gregs.dbvolution.results.AnyResult;
@@ -28,13 +26,10 @@ import nz.co.gregs.dbvolution.results.AnyResult;
  * LargeObjectExpression exposes database expressions for manipulating BLOBs,
  * CLOBs, and JavaObjects.
  *
- * <p style="color: #F90;">Support DBvolution at
- * <a href="http://patreon.com/dbvolution" target=new>Patreon</a></p>
- *
  * @author gregorygraham
- * @param <BASETYPE>
- * @param <RESULT>
- * @param <QDT>
+ * @param <BASETYPE> the fundamental Java type used with this LargeObjectExpression
+ * @param <RESULT> the Result class used to represent this kind of LargeObjectExpression
+ * @param <QDT> the QDT used to represent this values of this LargeObjectExpression
  */
 public abstract class LargeObjectExpression<BASETYPE extends Object, RESULT extends AnyResult<BASETYPE>, QDT extends QueryableDatatype<BASETYPE>>
 		extends AnyExpression<BASETYPE, RESULT, QDT>
@@ -42,8 +37,6 @@ public abstract class LargeObjectExpression<BASETYPE extends Object, RESULT exte
 
 	private final static long serialVersionUID = 1l;
 
-//	private final LargeObjectResult blobResult;
-//	private final boolean nullProtectionRequired;
 	/**
 	 * Default Constructor.
 	 */
@@ -59,8 +52,6 @@ public abstract class LargeObjectExpression<BASETYPE extends Object, RESULT exte
 	 */
 	public LargeObjectExpression(RESULT originalBlob) {
 		super(originalBlob);
-//		blobResult = originalBlob;
-//		nullProtectionRequired = originalBlob == null || originalBlob.getIncludesNull();
 	}
 
 	@Override
@@ -68,66 +59,6 @@ public abstract class LargeObjectExpression<BASETYPE extends Object, RESULT exte
 		return getInnerResult().toSQLString(db);
 	}
 
-//	@Override
-//	public LargeObjectExpression<BASETYPE,RESULT,QDT> copy() {
-//		return new LargeObjectExpression(blobResult==null?null:blobResult.copy());
-//	}
-//	@Override
-//	public DBLargeBinary getQueryableDatatypeForExpressionValue() {
-//		return new DBLargeBinary();
-//	}
-//	@Override
-//	public boolean isAggregator() {
-//		return blobResult.isAggregator();
-//	}
-//	@Override
-//	public Set<DBRow> getTablesInvolved() {
-//		HashSet<DBRow> hashSet = new HashSet<DBRow>();
-//		if (blobResult != null) {
-//			hashSet.addAll(blobResult.getTablesInvolved());
-//		}
-//		return hashSet;
-//	}
-	/**
-	 * Tests the LargeObjectExpression to see if it is not NULL in the database.
-	 *
-	 * <p style="color: #F90;">Support DBvolution at
-	 * <a href="http://patreon.com/dbvolution" target=new>Patreon</a></p>
-	 *
-	 * @return a BooleanExpression to use in {@link DBQuery#addCondition(nz.co.gregs.dbvolution.expressions.BooleanExpression)
-	 * }
-	 */
-//	public BooleanExpression isNotNull() {
-//		return BooleanExpression.isNotNull(this);
-//	}
-	/**
-	 * Tests the LargeObjectExpression to see if it is NULL in the database.
-	 *
-	 * <p style="color: #F90;">Support DBvolution at
-	 * <a href="http://patreon.com/dbvolution" target=new>Patreon</a></p>
-	 *
-	 * @return a BooleanExpression to use in {@link DBQuery#addCondition(nz.co.gregs.dbvolution.expressions.BooleanExpression)
-	 * }
-	 */
-//	public BooleanExpression isNull() {
-//		return BooleanExpression.isNull(this);
-//	}
-//	@Override
-//	public boolean getIncludesNull() {
-//		return nullProtectionRequired;
-//	}
-//	@Override
-//	public boolean isPurelyFunctional() {
-//		if (blobResult == null) {
-//			return true; // this should never occur, just sayin'
-//		} else {
-//			return blobResult.isPurelyFunctional();
-//		}
-//	}
-//	@Override
-//	public DBLargeBinary asExpressionColumn() {
-//		return new DBLargeBinary(this);
-//	}
 	@Override
 	public String createSQLForFromClause(DBDatabase database) {
 		throw new UnsupportedOperationException("LargeObjectExpresssion does not support createSQLForFromClause(DBDatabase) yet."); //To change body of generated methods, choose Tools | Templates.
@@ -137,19 +68,6 @@ public abstract class LargeObjectExpression<BASETYPE extends Object, RESULT exte
 	public boolean isComplexExpression() {
 		return false;
 	}
-
-//	@Override
-//	public LargeObjectResult expression(byte[] value) {
-//		return expression(new DBLargeBinary(value));
-//	}
-//	@Override
-//	public LargeObjectResult expression(LargeObjectResult value) {
-//		return new LargeObjectExpression(value);
-//	}
-//	@Override
-//	public LargeObjectResult expression(DBLargeBinary value) {
-//		return new LargeObjectExpression(value);
-//	}
 
 	@Override
 	public boolean isPurelyFunctional() {
