4b18bc67d440fff5866b57ee3d1cc992785e8e69
Gregory Graham <gregory@gregs.co.nz>
2021-11-29 11:19:16 +1300

    Redesigned the parsing to use RegexValueFinder and RegexSplitter as it produces cleaner code JavaDoc updates and improvements

--- src/main/java/nz/co/gregs/dbvolution/internal/datatypes/DurationImpl.java
+++ src/main/java/nz/co/gregs/dbvolution/internal/datatypes/DurationImpl.java
@@ -19,17 +19,32 @@ import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import nz.co.gregs.regexi.Regex;
+import nz.co.gregs.regexi.RegexSplitter;
+import nz.co.gregs.regexi.RegexValueFinder;
 
 /**
  *
- * <p style="color: #F90;">Support DBvolution at
- * <a href="http://patreon.com/dbvolution" target=new>Patreon</a></p>
  *
  * @author gregorygraham
  */
 public class DurationImpl {
 
 	private static final String ZERO_DURATION_STRING = "P0DT0H0M0.0S";
+	private static final String DAYS_CAPTURE = "days";
+	private static final String HOURS_CAPTURE = "hours";
+	private static final String MINUTES_CAPTURE = "minutes";
+	private static final String SECONDS_CAPTURE = "seconds";
+	private static final Regex REGEX = Regex.empty()
+			.literal("P").beginNamedCapture(DAYS_CAPTURE).numberLike().endNamedCapture()
+			.literal("DT").beginNamedCapture(HOURS_CAPTURE).numberLike().endNamedCapture()
+			.literal("H").beginNamedCapture(MINUTES_CAPTURE).numberLike().endNamedCapture()
+			.literal("M").beginNamedCapture(SECONDS_CAPTURE).numberLike().endNamedCapture()
+			.literal("S").toRegex();
+	private static final RegexValueFinder GET_DAYS = REGEX.returnValueFrom(DAYS_CAPTURE);
+	private static final RegexValueFinder GET_HOURS = REGEX.returnValueFrom(HOURS_CAPTURE);
+	private static final RegexValueFinder GET_MINUTES = REGEX.returnValueFrom(MINUTES_CAPTURE);
+	private static final RegexValueFinder GET_SECONDS = REGEX.returnValueFrom(SECONDS_CAPTURE);
 
 	/**
 	 * Default constructor
@@ -40,8 +55,6 @@ public class DurationImpl {
 
 	/**
 	 *
-	 * <p style="color: #F90;">Support DBvolution at
-	 * <a href="http://patreon.com/dbvolution" target=new>Patreon</a></p>
 	 *
 	 * @return the DateRepeat version of Zero
 	 */
@@ -62,31 +75,29 @@ public class DurationImpl {
 			return null;
 		}
 		final Instant originalInstant = original.toInstant(ZoneOffset.UTC);
-		double originalTime = 0.0d +originalInstant.getEpochSecond()+((0.0d+originalInstant.getNano())/1000000000.0d);
+		double originalTime = 0.0d + originalInstant.getEpochSecond() + ((0.0d + originalInstant.getNano()) / 1000000000.0d);
 		final Instant compareInstant = compareTo.toInstant(ZoneOffset.UTC);
-		double compareTime = 0.0d +compareInstant.getEpochSecond()+((0.0d+compareInstant.getNano())/1000000000.0d);
-		double differenceInSeconds = originalTime-compareTime;
-		int days = (int) (differenceInSeconds/(SECONDS_IN_A_DAY));
-		differenceInSeconds -= days*SECONDS_IN_A_DAY;
-		int hours = (int) (differenceInSeconds/(SECONDS_IN_AN_HOUR));
-		differenceInSeconds -= hours*SECONDS_IN_AN_HOUR;
-		int minutes = (int) (differenceInSeconds/(SECONDS_IN_A_MINUTE));
-		differenceInSeconds -= minutes*SECONDS_IN_A_MINUTE;
-		int seconds =  (int) (differenceInSeconds);
+		double compareTime = 0.0d + compareInstant.getEpochSecond() + ((0.0d + compareInstant.getNano()) / 1000000000.0d);
+		double differenceInSeconds = originalTime - compareTime;
+		int days = (int) (differenceInSeconds / (SECONDS_IN_A_DAY));
+		differenceInSeconds -= days * SECONDS_IN_A_DAY;
+		int hours = (int) (differenceInSeconds / (SECONDS_IN_AN_HOUR));
+		differenceInSeconds -= hours * SECONDS_IN_AN_HOUR;
+		int minutes = (int) (differenceInSeconds / (SECONDS_IN_A_MINUTE));
+		differenceInSeconds -= minutes * SECONDS_IN_A_MINUTE;
+		int seconds = (int) (differenceInSeconds);
 
 		String intervalString = "P" + days + "DT" + hours + "H" + minutes + "M" + seconds + "S";
 		return intervalString;
 	}
 	private static final int SECONDS_IN_A_MINUTE = 60;
-	private static final int SECONDS_IN_AN_HOUR = 60*60;
-	private static final int SECONDS_IN_A_DAY = 24*60*60;
+	private static final int SECONDS_IN_AN_HOUR = 60 * 60;
+	private static final int SECONDS_IN_A_DAY = 24 * 60 * 60;
 
 	/**
 	 *
-	 * @param interval
-	 * <p style="color: #F90;">Support DBvolution at
-	 * <a href="http://patreon.com/dbvolution" target=new>Patreon</a></p>
-	 * @return the DateRepeat equivalent of the Period value
+	 * @param interval the Duration to convert to DBDuration's string format
+	 * @return the DBDuration equivalent of the Duration value
 	 */
 	public static String getDurationString(Duration interval) {
 		if (interval == null) {
@@ -97,7 +108,7 @@ public class DurationImpl {
 		int minutes = interval.toMinutesPart();
 
 		int nanos = interval.toNanosPart();
-		double seconds = interval.toSecondsPart()+ (nanos / 1000000000.0);
+		double seconds = interval.toSecondsPart() + (nanos / 1000000000.0);
 		String intervalString = "P" + days + "DT" + hours + "H" + minutes + "M" + seconds + "S";
 		return intervalString;
 	}
@@ -137,6 +148,8 @@ public class DurationImpl {
 		return compareDurationStrings(original, compareTo) == -1;
 	}
 
+	private static final RegexSplitter SPLIT_ON_LETTERS = Regex.empty().beginSetIncluding().includeLetters().endSet().toSplitter();
+
 	/**
 	 *
 	 * @param original the first date
@@ -149,8 +162,8 @@ public class DurationImpl {
 		if (original == null || compareTo == null) {
 			return null;
 		}
-		String[] splitOriginal = original.split("[A-Za-z]");
-		String[] splitCompareTo = compareTo.split("[A-Za-z]");
+		String[] splitOriginal = SPLIT_ON_LETTERS.split(original);
+		String[] splitCompareTo = SPLIT_ON_LETTERS.split(compareTo);
 		for (int i = 1; i < splitCompareTo.length; i++) { // Start at 1 because the first split is empty
 			double intOriginal = Double.parseDouble(splitOriginal[i]);
 			double intCompareTo = Double.parseDouble(splitCompareTo[i]);
@@ -203,7 +216,7 @@ public class DurationImpl {
 		if (original == null || intervalInput == null || intervalInput.length() == 0) {
 			return null;
 		}
-		
+
 		int days = getDayPart(intervalInput);
 		int hours = getHourPart(intervalInput);
 		int minutes = getMinutePart(intervalInput);
@@ -231,13 +244,6 @@ public class DurationImpl {
 		}
 
 		Duration interval = Duration.parse(intervalStr);
-//		interval = interval.withYears(getYearPart(intervalStr));
-//		interval = interval.withMonths(getMonthPart(intervalStr));
-//		interval = interval.withDays(getDayPart(intervalStr));
-//		interval = interval.withHours(getHourPart(intervalStr));
-//		interval = interval.withMinutes(getMinutePart(intervalStr));
-//		interval = interval.withSeconds(getSecondPart(intervalStr));
-//		interval = interval.withMillis(getMillisecondPart(intervalStr));
 		return interval;
 	}
 
@@ -246,13 +252,17 @@ public class DurationImpl {
 	 * @param intervalStr the DateRepeat
 	 *
 	 * @return get the fractional seconds to millisecond precision
-	 * @throws NumberFormatException
+	 * @throws NumberFormatException it's possible that the numbers are malformed 
 	 */
 	public static Integer getNanoPart(String intervalStr) throws NumberFormatException {
 		if (intervalStr == null || intervalStr.length() == 0) {
 			return null;
 		}
-		final Double secondsDouble = Double.parseDouble(intervalStr.replaceAll(".*M([-0-9.]+)S.*", "$1"));
+		final String stringValue = GET_SECONDS.getValueFrom(intervalStr).orElse("");
+		if (stringValue.isEmpty()) {
+			return null;
+		}
+		final Double secondsDouble = Double.parseDouble(stringValue);
 		final int secondsInt = secondsDouble.intValue();
 		final Double nanoDouble = secondsDouble * 1000000000.0 - secondsInt * 1000000000;
 		final int nanos = nanoDouble.intValue();
@@ -264,13 +274,17 @@ public class DurationImpl {
 	 * @param intervalStr the DateRepeat
 	 *
 	 * @return get the integer and fractional seconds part of the DateRepeat
-	 * @throws NumberFormatException
+	 * @throws NumberFormatException it's possible that the numbers are malformed 
 	 */
 	public static Integer getSecondPart(String intervalStr) throws NumberFormatException {
-		if (intervalStr == null || intervalStr.length() == 0 || !intervalStr.matches(".*M([-0-9.]+)S.*")) {
+		if (intervalStr == null || intervalStr.length() == 0 || !REGEX.matchesEntireString(intervalStr)) {
 			return null;
 		}
-		final Double valueOf = Double.valueOf(intervalStr.replaceAll(".*M([-0-9.]+)S.*", "$1"));
+		final String stringValue = GET_SECONDS.getValueFrom(intervalStr).orElse("");
+		if (stringValue.isEmpty()) {
+			return null;
+		}
+		final Double valueOf = Double.valueOf(stringValue);
 		return valueOf.intValue();
 	}
 
@@ -279,13 +293,17 @@ public class DurationImpl {
 	 * @param intervalStr the DateRepeat
 	 *
 	 * @return get the minutes part of the DateRepeat
-	 * @throws NumberFormatException
+	 * @throws NumberFormatException it's possible that the numbers are malformed 
 	 */
 	public static Integer getMinutePart(String intervalStr) throws NumberFormatException {
-		if (intervalStr == null || intervalStr.length() == 0) {
+		if (intervalStr == null || intervalStr.length() == 0 || !REGEX.matchesEntireString(intervalStr)) {
 			return null;
 		}
-		return Integer.parseInt(intervalStr.replaceAll(".*H([-0-9.]+)M.*", "$1"));
+		final String stringValue = GET_MINUTES.getValueFrom(intervalStr).orElse("");
+		if (stringValue.isEmpty()) {
+			return null;
+		}
+		return Integer.parseInt(stringValue);
 	}
 
 	/**
@@ -293,13 +311,17 @@ public class DurationImpl {
 	 * @param intervalStr the DateRepeat
 	 *
 	 * @return get the hour part of the DateRepeat value
-	 * @throws NumberFormatException
+	 * @throws NumberFormatException it's possible that the numbers are malformed 
 	 */
 	public static Integer getHourPart(String intervalStr) throws NumberFormatException {
-		if (intervalStr == null || intervalStr.length() == 0) {
+		if (intervalStr == null || intervalStr.length() == 0 || !REGEX.matchesEntireString(intervalStr)) {
 			return null;
 		}
-		return Integer.parseInt(intervalStr.replaceAll(".*DT([-0-9.]+)H.*", "$1"));
+		final String stringValue = GET_HOURS.getValueFrom(intervalStr).orElse("");
+		if (stringValue.isEmpty()) {
+			return null;
+		}
+		return Integer.parseInt(stringValue);
 	}
 
 	/**
@@ -307,12 +329,16 @@ public class DurationImpl {
 	 * @param intervalStr the DateRepeat
 	 *
 	 * @return get the day part of the DateRepeat value
-	 * @throws NumberFormatException
+	 * @throws NumberFormatException it's possible that the numbers are malformed 
 	 */
 	public static Integer getDayPart(String intervalStr) throws NumberFormatException {
-		if (intervalStr == null || intervalStr.length() == 0) {
+		if (intervalStr == null || intervalStr.length() == 0 || !REGEX.matchesEntireString(intervalStr)) {
 			return null;
 		}
-		return Integer.parseInt(intervalStr.replaceAll(".*P([-0-9.]+)DT.*", "$1"));
+		final String stringValue = GET_DAYS.getValueFrom(intervalStr).orElse("");
+		if (stringValue.isEmpty()) {
+			return null;
+		}
+		return Integer.parseInt(stringValue);
 	}
 }
