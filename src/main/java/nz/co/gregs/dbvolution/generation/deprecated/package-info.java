4d1337545e76212d36e421589d1361846dd8d53f
Gregory Graham <gregory@gregs.co.nz>
2021-11-29 16:26:14 +1300

    JavaDoc updates and improvements

--- src/main/java/nz/co/gregs/dbvolution/generation/deprecated/package-info.java
+++ src/main/java/nz/co/gregs/dbvolution/generation/deprecated/package-info.java
@@ -6,9 +6,9 @@
  * <p>
  * With the the DBDatabase you can create to DBRow classes required from the
  * database schema using
- * {@link nz.co.gregs.dbvolution.generation.DBTableClassGenerator} or you can
+ * {@link nz.co.gregs.dbvolution.generation.deprecated.DBTableClassGenerator} or you can
  * write you own. There are examples to start from in
- * {@link nz.co.gregs.dbvolution.example}.
+ * nz.co.gregs.dbvolution.example package of the test folders
  * <p>
  * I've been writing SQL for 20 years and DBV is easier, I hope you like it.
  * <p>
