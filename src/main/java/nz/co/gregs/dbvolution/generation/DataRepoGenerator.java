a3e76b10eafd42db10b29ca70aa26b60cbdc2ab9
Gregory Graham <gregory@gregs.co.nz>
2023-05-25 17:33:16 +1200

    Added class to encapsulate all the metadata collection since it needs to include database system tables as well

--- src/main/java/nz/co/gregs/dbvolution/generation/DataRepoGenerator.java
+++ src/main/java/nz/co/gregs/dbvolution/generation/DataRepoGenerator.java
@@ -32,6 +32,7 @@ import nz.co.gregs.dbvolution.databases.DBDatabase;
 import nz.co.gregs.dbvolution.databases.DBDatabaseCluster;
 import nz.co.gregs.dbvolution.databases.DBStatement;
 import nz.co.gregs.dbvolution.datatypes.*;
+import nz.co.gregs.dbvolution.exceptions.NoAvailableDatabaseException;
 
 /**
  * Automatically generates Java files to be used in your data model.
@@ -44,9 +45,6 @@ import nz.co.gregs.dbvolution.datatypes.*;
  * @author Gregory Graham
  */
 class DataRepoGenerator {
-
-	private static final String[] JAVA_RESERVED_WORDS_ARRAY = new String[]{"null", "true", "false", "abstract", "continue", "for", "new", "switch", "assert", "default", "goto", "package", "synchronized", "boolean", "do", "if", "private", "this", "break", "double", "implements", "", "protected", "throw", "byte", "else", "import", "public", "throws", "case", "enum", "instanceof", "return", "transient", "catch", "extends", "int", "short", "try", "char", "final", "interface", "static", "", "void", "class", "finally", "long", "strictfp", "volatile", "const", "float", "native", "super", "while"};
-	private static final List<String> JAVA_RESERVED_WORDS = Arrays.asList(JAVA_RESERVED_WORDS_ARRAY);
 
 	private DataRepoGenerator() {
 	}
@@ -223,245 +221,15 @@ class DataRepoGenerator {
 	 * found on the database 1 Database exceptions may be thrown
 	 */
 	private static DataRepo parseObjectTypes(DBDatabase db, String packageName, Options options, String... dbObjectTypes) throws SQLException {
-		DataRepo datarepo = new DataRepo(db, packageName);
 
 		DBDatabase database = db;
-		if (db instanceof DBDatabaseCluster) {
+		while (db instanceof DBDatabaseCluster) {
 			database = ((DBDatabaseCluster) db).getReadyDatabase();
 		}
 
-		try (DBStatement dbStatement = database.getDBStatement()) {
-			DBConnection connection = dbStatement.getConnection();
-			String catalog = connection.getCatalog();
-			String schema = null;
-			try {
-				//Method method = connection.getClass().getMethod("getSchema");
-				//schema = (String) method.invoke(connection);
-				schema = connection.getSchema();
-			} catch (java.sql.SQLFeatureNotSupportedException nope) {
-				// SOMEONE DIDN'T WRITE THEIR DRIVER PROPERLY
-			} catch (java.lang.AbstractMethodError exp) {
-				// NOT USING Java 1.7+ apparently
-			} catch (IllegalArgumentException ex) {
-				// NOT USING Java 1.7+ apparently
-			} catch (SecurityException ex) {
-				// NOT USING Java 1.7+ apparently
-			}
+		DataRepo datarepo = new DBDatabaseMetaData(database, dbObjectTypes, packageName, options).getDataRepo();
 
-			DatabaseMetaData metaData = connection.getMetaData();
-			try (ResultSet tables = metaData.getTables(catalog, schema, null, dbObjectTypes)) {
-				while (tables.next()) {
-					final String tableName = tables.getString("TABLE_NAME");
-					if (schema == null) {
-						schema = tables.getString("TABLE_SCHEM");
-					}
-					if (tableName.matches(database.getDefinition().getSystemTableExclusionPattern())) {
-						final String className = toClassCase(tableName);
-						DBTableClass dbTableClass = new DBTableClass(tableName, schema, packageName, className);
-
-						ResultSet primaryKeysRS = metaData.getPrimaryKeys(catalog, schema, dbTableClass.getTableName());
-						List<String> pkNames = new ArrayList<>();
-						try {
-							while (primaryKeysRS.next()) {
-								String pkColumnName = primaryKeysRS.getString("COLUMN_NAME");
-								pkNames.add(pkColumnName);
-							}
-						} finally {
-							primaryKeysRS.close();
-						}
-
-						ResultSet foreignKeysRS = metaData.getImportedKeys(catalog, schema, dbTableClass.getTableName());
-						Map<String, String[]> fkNames = new HashMap<>();
-						try {
-							while (foreignKeysRS.next()) {
-								String pkTableName = foreignKeysRS.getString("PKTABLE_NAME");
-								String pkColumnName = foreignKeysRS.getString("PKCOLUMN_NAME");
-								String fkColumnName = foreignKeysRS.getString("FKCOLUMN_NAME");
-								fkNames.put(fkColumnName, new String[]{pkTableName, pkColumnName});
-							}
-						} finally {
-							foreignKeysRS.close();
-						}
-
-						try (ResultSet columns = metaData.getColumns(catalog, schema, dbTableClass.getTableName(), null)) {
-							while (columns.next()) {
-								DBTableField dbTableField = new DBTableField();
-								dbTableField.columnName = columns.getString("COLUMN_NAME");
-								dbTableField.fieldName = toFieldCase(dbTableField.columnName);
-								try {
-									dbTableField.referencedTable = columns.getString("SCOPE_TABLE");
-								} catch (SQLException exp) {
-									; // MSSQLServer throws an exception on this
-								}
-								dbTableField.precision = columns.getInt("COLUMN_SIZE");
-								dbTableField.comments = columns.getString("REMARKS");
-								String isAutoIncr = null;
-								try {
-									isAutoIncr = columns.getString("IS_AUTOINCREMENT");
-								} catch (SQLException sqlex) {
-									;// SQLite-JDBC throws an exception when retrieving IS_AUTOINCREMENT
-								}
-								dbTableField.isAutoIncrement = isAutoIncr != null && isAutoIncr.equals("YES");
-								try {
-									dbTableField.sqlDataTypeInt = columns.getInt("DATA_TYPE");
-									dbTableField.sqlDataTypeName = columns.getString("TYPE_NAME");
-									dbTableField.columnType = getQueryableDatatypeNameOfSQLType(database, dbTableField, options.getTrimCharColumns());
-								} catch (UnknownJavaSQLTypeException ex) {
-									dbTableField.columnType = DBUnknownDatatype.class;
-									dbTableField.javaSQLDatatype = ex.getUnknownJavaSQLType();
-								}
-								if (pkNames.contains(dbTableField.columnName) || options.getPkRecog().isPrimaryKeyColumn(dbTableClass.getTableName(), dbTableField.columnName)) {
-									dbTableField.isPrimaryKey = true;
-								}
-
-								String[] pkData = fkNames.get(dbTableField.columnName);
-								if (pkData != null && pkData.length == 2) {
-									dbTableField.isForeignKey = true;
-									dbTableField.referencesClass = toClassCase(pkData[0]);
-									dbTableField.referencesField = pkData[1];
-								} else if (options.getFkRecog().isForeignKeyColumn(dbTableClass.getTableName(), dbTableField.columnName)) {
-									dbTableField.isForeignKey = true;
-									dbTableField.referencesField = options.getFkRecog().getReferencedColumn(dbTableClass.getTableName(), dbTableField.columnName);
-									dbTableField.referencesClass = toClassCase(options.getFkRecog().getReferencedTable(dbTableClass.getTableName(), dbTableField.columnName));
-								}
-								if (!dbTableClass.getFields().contains(dbTableField)) {
-									dbTableClass.getFields().add(dbTableField);
-								}
-							}
-						}
-						datarepo.addTable(dbTableClass);
-					}
-				}
-			}
-			connectForeignKeyReferences(datarepo.getTables(), options);
-		}
 		return datarepo;
-	}
-
-	private static void connectForeignKeyReferences(List<DBTableClass> dbTableClasses, Options options) {
-		List<String> dbTableClassNames = new ArrayList<>();
-
-		for (DBTableClass dbt : dbTableClasses) {
-			dbTableClassNames.add(dbt.getClassName());
-		}
-		for (DBTableClass dbt : dbTableClasses) {
-			for (DBTableField dbf : dbt.getFields()) {
-				if (dbf.isForeignKey) {
-					if (!dbTableClassNames.contains(dbf.referencesClass)) {
-						List<String> matchingNames = new ArrayList<>();
-						for (String name : dbTableClassNames) {
-							if (name.toLowerCase().startsWith(dbf.referencesClass.toLowerCase())) {
-								matchingNames.add(name);
-							}
-						}
-						if (matchingNames.size() == 1) {
-							String properClassname = matchingNames.get(0);
-							dbf.referencesClass = properClassname;
-						}
-					}
-				}
-			}
-		}
-	}
-
-	/**
-	 *
-	 * Returns a string of the appropriate QueryableDatatype for the specified
-	 * SQLType
-	 *
-	 *
-	 *
-	 *
-	 *
-	 * @return a string of the appropriate QueryableDatatype for the specified
-	 * SQLType
-	 */
-	private static Class<? extends Object> getQueryableDatatypeNameOfSQLType(DBDatabase database, DBTableField column, Boolean trimCharColumns) throws UnknownJavaSQLTypeException {
-		int columnType = column.sqlDataTypeInt;
-		int precision = column.precision;
-		String typeName = column.sqlDataTypeName;
-
-		Class<? extends Object> value;
-		switch (columnType) {
-			case Types.BIT:
-				if (precision == 1) {
-					value = DBBoolean.class;
-				} else {
-					value = DBLargeBinary.class;
-				}
-				break;
-			case Types.TINYINT:
-			case Types.INTEGER:
-			case Types.BIGINT:
-			case Types.BOOLEAN:
-			case Types.ROWID:
-			case Types.SMALLINT:
-				if (precision == 1) {
-					value = DBBoolean.class;
-				} else {
-					value = DBInteger.class;
-				}
-				break;
-			case Types.DECIMAL:
-			case Types.DOUBLE:
-			case Types.FLOAT:
-			case Types.NUMERIC:
-			case Types.REAL:
-				value = DBNumber.class;
-				break;
-			case Types.CHAR:
-			case Types.NCHAR:
-				if (trimCharColumns) {
-					value = DBStringTrimmed.class;
-				} else {
-					value = DBString.class;
-				}
-				break;
-			case Types.VARCHAR:
-			case Types.NVARCHAR:
-			case Types.LONGNVARCHAR:
-			case Types.LONGVARCHAR:
-				value = DBString.class;
-				break;
-			case Types.DATE:
-			case Types.TIME:
-			case Types.TIMESTAMP:
-				value = DBDate.class;
-				break;
-			case Types.OTHER:
-				Class<? extends QueryableDatatype<?>> customType = database.getDefinition().getQueryableDatatypeClassForSQLDatatype(typeName);
-				if (customType != null) {
-					value = customType;
-					break;
-				} else {
-					value = DBJavaObject.class;
-					break;
-				}
-			case Types.JAVA_OBJECT:
-				value = DBJavaObject.class;
-				break;
-			case Types.CLOB:
-			case Types.NCLOB:
-				value = DBLargeText.class;
-				break;
-			case Types.BINARY:
-			case Types.VARBINARY:
-			case Types.LONGVARBINARY:
-			case Types.BLOB:
-			case Types.ARRAY:
-			case Types.SQLXML:
-				Class<? extends QueryableDatatype<?>> customBinaryType = database.getDefinition().getQueryableDatatypeClassForSQLDatatype(typeName);
-				if (customBinaryType != null) {
-					value = customBinaryType;
-					break;
-				} else {
-				value = DBLargeBinary.class;
-					break;
-				}
-			default:
-				throw new UnknownJavaSQLTypeException("Unknown Java SQL Type: " + columnType, columnType);
-		}
-		return value;
 	}
 
 	/**
@@ -488,28 +256,6 @@ class DataRepoGenerator {
 			}
 		}
 		return classCaseString.toString();
-	}
-
-	/**
-	 *
-	 * returns a good guess at the java field version of a DB field name.
-	 *
-	 * I.e. changes "_" into an uppercase letter.
-	 *
-	 *
-	 *
-	 *
-	 *
-	 * @return Camel Case version of S
-	 */
-	private static String toFieldCase(String s) {
-		String classClass = toClassCase(s);
-		String camelCaseString = classClass.substring(0, 1).toLowerCase() + classClass.substring(1);
-		camelCaseString = camelCaseString.replaceAll("[^a-zA-Z0-9_$]", "_");
-		if (JAVA_RESERVED_WORDS.contains(camelCaseString)) {
-			camelCaseString += "_";
-		}
-		return camelCaseString;
 	}
 
 	/**
