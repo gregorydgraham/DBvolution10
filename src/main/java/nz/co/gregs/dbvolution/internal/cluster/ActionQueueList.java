f7ca8ca6d13e1c62e7e6e5de9f701f5c7ad6e356
Gregory Graham <gregory@gregs.co.nz>
2023-09-16 18:18:31 +1200

    Added tests for ActionQueueList

--- src/main/java/nz/co/gregs/dbvolution/internal/cluster/ActionQueueList.java
+++ src/main/java/nz/co/gregs/dbvolution/internal/cluster/ActionQueueList.java
@@ -31,10 +31,15 @@
 package nz.co.gregs.dbvolution.internal.cluster;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import nz.co.gregs.dbvolution.actions.DBAction;
 import nz.co.gregs.dbvolution.databases.DBDatabase;
 import nz.co.gregs.dbvolution.internal.database.ClusterDetails;
+import nz.co.gregs.looper.LoopVariable;
 
 /**
  *
@@ -44,32 +49,71 @@ public class ActionQueueList implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private final HashMap<DBDatabase, ActionQueue> queues = new HashMap<>(2);
+	private final HashMap<String, ActionQueue> queues = new HashMap<>(2);
 	private final ClusterDetails clusterDetails;
+	private final int maxQueueSize = 100000;
+	private transient final Object A_QUEUE_IS_READY = new Object();
 
 	public ActionQueueList(ClusterDetails clusterDetails) {
 		this.clusterDetails = clusterDetails;
 	}
 
-	public synchronized ActionQueue add(DBDatabase db) {
-		final ActionQueue actionQueue = new ActionQueue(db, clusterDetails, 100000);
-		actionQueue.start();
-		queues.put(db, actionQueue);
-		return actionQueue;
+	private ActionQueue getNewActionQueue(DBDatabase db) {
+		return new ActionQueue(db, clusterDetails, maxQueueSize, this);
+	}
+
+	public synchronized String add(DBDatabase db) {
+		final String key = getKey(db);
+		final ActionQueue queue = getNewActionQueue(db);
+		return add(key, queue);
+	}
+
+	private synchronized String add(String key, ActionQueue queue) {
+		queues.put(key, queue);
+		queue.start();
+		return key;
+	}
+
+	public void start(DBDatabase db) {
+		ActionQueue queueForDatabase = getQueueForDatabase(db);
+		queueForDatabase.start();
+	}
+
+	public synchronized int size() {
+		return queues.size();
+	}
+
+	public synchronized void queueAction(DBDatabase db, DBAction act) {
+		final String key = getKey(db);
+		ActionQueue queue = queues.get(key);
+		if (queue == null) {
+			queue = getNewActionQueue(db);
+			add(key, queue);
+		}
+		queue.add(act);
+	}
+
+	public synchronized void queueActionForAllDatabases(DBAction action) {
+		Collection<ActionQueue> actionQueues = queues.values();
+		for (ActionQueue actionQueue : actionQueues) {
+			actionQueue.add(action);
+		}
+	}
+
+	public synchronized void queueActionForAllDatabases(DBAction... actions) {
+		for (DBAction action : actions) {
+			queueActionForAllDatabases(action);
+		}
 	}
 
 	public synchronized ActionQueue remove(DBDatabase db) {
-		ActionQueue found = queues.get(db);
+		final String key = getKey(db);
+		ActionQueue found = queues.get(key);
 		if (found != null) {
 			found.stop();
-			queues.remove(db);
+			queues.remove(key);
 		}
 		return found;
-	}
-
-	public synchronized ActionQueue remove(ActionQueue q) {
-		queues.remove(q.getDatabase());
-		return q;
 	}
 
 	public synchronized void clear() {
@@ -79,10 +123,143 @@ public class ActionQueueList implements Serializable {
 		queues.clear();
 	}
 
-	public void enqueue(DBDatabase database, DBAction action) {
-		final ActionQueue queue = queues.get(database);
-		if (queue != null) {
-			queue.add(action);
+	public boolean waitUntilAllQueuesAreEmpty() {
+		try {
+			Collection<ActionQueue> values = queues.values();
+			for (ActionQueue queue : values) {
+				queue.waitUntilEmpty();
+			}
+			return true;
+		} catch (Exception ex) {
+			LOG.log(Level.SEVERE, null, ex);
+			return false;
 		}
+	}
+
+	public boolean waitUntilAQueueIsReady() {
+		synchronized (A_QUEUE_IS_READY) {
+			try {
+				A_QUEUE_IS_READY.wait();
+				return true;
+			} catch (InterruptedException ex) {
+				LOG.log(Level.SEVERE, null, ex);
+				return false;
+			}
+		}
+	}
+	private static final Logger LOG = Logger.getLogger(ActionQueueList.class.getName());
+
+	public boolean waitUntilAQueueIsReady(long millisecondsToWait) {
+		Optional<ActionQueue> found = queues.values().stream().filter((t) -> t.isEmpty()).findAny();
+		if (found.isPresent()) {
+			return true;
+		}
+		synchronized (A_QUEUE_IS_READY) {
+			try {
+				A_QUEUE_IS_READY.wait(millisecondsToWait);
+				return true;
+			} catch (InterruptedException ex) {
+				LOG.log(Level.SEVERE, null, ex);
+				return false;
+			}
+		}
+	}
+
+	void notifyQueueIsReady() {
+		synchronized (A_QUEUE_IS_READY) {
+			A_QUEUE_IS_READY.notifyAll();
+		}
+	}
+
+	public boolean waitUntilReady(DBDatabase db) {
+		ActionQueue queue = queues.get(getKey(db));
+		if (queue != null) {
+			queue.waitUntilReady();
+			return true;
+		}
+		return false;
+	}
+
+	public boolean waitUntilReady(DBDatabase db, long milliseconds) {
+		ActionQueue queue = queues.get(getKey(db));
+		if (queue == null) {
+			return false;
+		}
+		queue.waitUntilEmpty(milliseconds);
+		return true;
+
+	}
+
+	public void pause(DBDatabase db) {
+		ActionQueue q = queues.get(getKey(db));
+		q.pause();
+	}
+
+	public void unpause(DBDatabase db) {
+		ActionQueue q = queues.get(getKey(db));
+		q.unpause();
+	}
+
+	public void unpause(DBDatabase... dbs) {
+		for (DBDatabase db : dbs) {
+			unpause(db);
+		}
+	}
+
+	public String getKey(DBDatabase db) {
+		return db.getSettings().encode();
+	}
+
+	public synchronized void copyFromTo(DBDatabase template, DBDatabase secondary) {
+		ActionQueue templateQ = queues.get(getKey(template));
+		ActionQueue secondaryQ = queues.get(getKey(secondary));
+		boolean templatePaused = templateQ.isPaused();
+		boolean secondaryPaused = secondaryQ.isPaused();
+		if (!templatePaused) {
+			templateQ.pause();
+		}
+		if (!secondaryPaused) {
+			secondaryQ.pause();
+		}
+		secondaryQ.clear();
+		secondaryQ.addAll(templateQ);
+		if (!secondaryPaused) {
+			secondaryQ.unpause();
+		}
+		if (!templatePaused) {
+			templateQ.unpause();
+		}
+	}
+
+	ActionQueue getQueueForDatabase(DBDatabase database) {
+		String key = getKey(database);
+		ActionQueue queue = queues.get(key);
+		if (queue == null) {
+			key = add(database);
+		}
+		queue = queues.get(key);
+		return queue;
+	}
+
+	void pause(DBDatabase... dbs) {
+		for (DBDatabase db : dbs) {
+			pause(db);
+		}
+	}
+
+	void add(DBDatabase... dbs) {
+		for (DBDatabase db : dbs) {
+			add(db);
+		}
+	}
+
+	ActionQueue[] getQueueForDatabase(DBDatabase... dbs) {
+		ActionQueue[] result = new ActionQueue[dbs.length];
+		LoopVariable loop = LoopVariable.factory(dbs.length);
+		for (DBDatabase db : dbs) {
+			result[loop.attempts()] = getQueueForDatabase(db);
+			loop.attempt();
+		}
+		return result;
 	}
 }
